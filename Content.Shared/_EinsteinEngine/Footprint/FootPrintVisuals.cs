360bd8a835e369848942e2d7bd76a38634cdc2b6
Alex C <alex91905@yahoo.com>
2024-11-13 20:01:30 -0500

    Adds footprint and drag mark system from Einstein engine (with fixes to make it not murder the server)

---
+++ Content.Shared/_EinsteinEngine/Footprint/FootPrintVisuals.cs
@@ -0,0 +1,25 @@
+using Robust.Shared.Serialization;
+
+namespace Content.Shared._EinsteinEngine.Footprint;
+
+[Serializable, NetSerializable]
+public enum FootPrintVisuals : byte
+{
+    BareFootPrint,
+    ShoesPrint,
+    SuitPrint,
+    Dragging
+}
+
+[Serializable, NetSerializable]
+public enum FootPrintVisualState : byte
+{
+    State,
+    Color
+}
+
+[Serializable, NetSerializable]
+public enum FootPrintVisualLayers : byte
+{
+    Print
+}
