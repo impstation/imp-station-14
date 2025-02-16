8df624946d93d3b7d5e6f9a0b2fd5b383fbe58e5
Saeko-44 <impassivedevil@gmail.com>
2025-02-16 13:04:53 +0100

    Item miners can now print something different if emagged.

--- Content.Server/_Impstation/Power/ItemMinerComponent.cs
+++ Content.Server/_Impstation/Power/ItemMinerComponent.cs
@@ -10,25 +10,38 @@ namespace Content.Server._Impstation.Power
     [RegisterComponent, AutoGenerateComponentPause]
     public sealed partial class ItemMinerComponent : Component
     {
-        /// <summary>
-        /// Charge threashold at which charge is converted into spesos.
-        /// </summary>
-        [DataField]
-        public float ChargeThreashold = 10000000;
-
-        /// <summary>
-        /// Item created when the threashold is reached.
-        /// </summary>
-        [DataField(required: true)]
-        public string ItemConvertion;
+        [DataField(customTypeSerializer: typeof(TimeOffsetSerializer))]
+        public TimeSpan NextSecond;
 
         /// <summary>
         /// The amount of energy produced each second when mining.
         /// </summary>
         [DataField, ViewVariables(VVAccess.ReadWrite)]
-        public float EnergyPerSecond = 1000;
+        public float EnergyPerSecond = 10000;
 
-        [DataField(customTypeSerializer: typeof(TimeOffsetSerializer))]
-        public TimeSpan NextSecond;
+        /// <summary>
+        /// Charge threashold at which charge is converted into spesos.
+        /// </summary>
+        [DataField]
+        public float ChargeThreashold = 1000000000;
+
+        /// <summary>
+        /// Id of the item created when the threashold is reached.
+        /// </summary>
+        [DataField(required: true)]
+        public string ItemConvertion;
+
+        /// <summary>
+        /// Id of the item created when the threashold is reached after being emagged.
+        /// If left empty, cannot be emagged.
+        /// </summary>
+        [DataField]
+        public string EmagConvertion = string.Empty;
+
+        /// <summary>
+        /// Item created when the threashold is reached after being emagged.
+        /// </summary>
+        [DataField]
+        public bool IsEmagged = false;
     }
 }
--- Content.Server/_Impstation/Power/ItemMinerSystem.cs
+++ Content.Server/_Impstation/Power/ItemMinerSystem.cs
@@ -4,6 +4,7 @@ using Content.Server.Power.EntitySystems;
 using Content.Shared.Atmos;
 using Robust.Server.GameObjects;
 using Content.Server.Atmos.EntitySystems;
+using Content.Shared.Emag.Systems;
 
 namespace Content.Server._Impstation.Power
 {
@@ -13,11 +14,19 @@ namespace Content.Server._Impstation.Power
         [Dependency] private readonly TransformSystem _transform = default!;
         [Dependency] private readonly BatterySystem _battery = default!;
         [Dependency] private readonly IGameTiming _timing = default!;
+        [Dependency] private readonly EmagSystem _emag = default!;
 
         /// <summary>
         /// Per-tick cache
         /// </summary>
         private readonly List<GasMixture> _environments = new();
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            SubscribeLocalEvent<ItemMinerComponent, GotEmaggedEvent>(OnEmagged);
+        }
 
         public override void Update(float frameTime)
         {
@@ -65,18 +74,44 @@ namespace Content.Server._Impstation.Power
                 }
                 #endregion
 
+                #region power to item convertion
                 //We add the power it drains from the grid to its battery
                 var newCharge = battery.CurrentCharge + networkLoad.NetworkLoad.ReceivingPower;
                 while (newCharge > component.ChargeThreashold)
                 {
                     //And if we're above the threadshold the battery gets turned into the mined item
-                    EntityManager.SpawnEntity(component.ItemConvertion, Transform(entity).Coordinates);
+                    if(component.IsEmagged) //Which item depends on if emagged or not
+                    {
+                        EntityManager.SpawnEntity(component.EmagConvertion, Transform(entity).Coordinates);
+                    }
+                    else
+                    {
+                        EntityManager.SpawnEntity(component.ItemConvertion, Transform(entity).Coordinates);
+                    }
                     newCharge -= component.ChargeThreashold;
                 }
 
                 //And we update the battery's charge
                 _battery.SetCharge(entity, newCharge, battery);
+                #endregion
             }
+        }
+
+        private void OnEmagged(EntityUid uid, ItemMinerComponent component, ref GotEmaggedEvent args)
+        {
+            if (!_emag.CompareFlag(args.Type, EmagType.Interaction))
+                return;
+
+            if (_emag.CheckFlag(uid, EmagType.Interaction))
+                return;
+
+            //Only trigger the emag if there is an emagConvertion for this miner
+            if(component.EmagConvertion != string.Empty)
+            {
+                component.IsEmagged = !component.IsEmagged;
+            }
+
+            args.Handled = true;
         }
     }
 }
