139e3b4959d5af0ff9c39872acf29c6c1df86fb1
Darkmajia <darkwindleaf@hotmail.co.uk>
2025-03-18 14:02:06 +0000

    remove dupe power grid check audio

--- Content.Server/StationEvents/Components/PowerGridCheckRuleComponent.cs
+++ Content.Server/StationEvents/Components/PowerGridCheckRuleComponent.cs
@@ -19,8 +19,6 @@ public sealed partial class PowerGridCheckRuleComponent : Component
     [DataField]
     public SoundSpecifier PowerOnSound = new SoundCollectionSpecifier(DefaultPowerOn, AudioParams.Default.WithVolume(-4f));
 
-    public CancellationTokenSource? AnnounceCancelToken;
-
     public EntityUid AffectedStation;
     public readonly List<EntityUid> Powered = new();
     public readonly List<EntityUid> Unpowered = new();
