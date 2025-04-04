8ba3661b74a14e5c81b3db6c3a7eb4b01927f697
FlippenPage <127570745+FlippenPage@users.noreply.github.com>
2025-04-04 16:39:35 -0700

    i thiiink this is how this was?

--- Content.Server/Mech/Systems/MechSystem.cs
+++ Content.Server/Mech/Systems/MechSystem.cs
@@ -205,13 +205,13 @@ public sealed partial class MechSystem : SharedMechSystem
                 Priority = 1, // Promote to top to make ejecting the ALT-click action
                 Act = () =>
                 {
-                    if (args.User == uid || args.User == component.PilotSlot.ContainedEntity)
+                    var doAfterEventArgs = new DoAfterArgs(EntityManager, args.User, component.ExitDelay, new MechExitEvent(), uid, target: uid)
                     {
-                        TryEject(uid, component);
-                        return;
-                    }
-
+                        BreakOnMove = true,
+                    };
                     _popup.PopupEntity(Loc.GetString("mech-eject-pilot-alert", ("item", uid), ("user", args.User)), uid, PopupType.Large);
+
+                    _doAfter.TryStartDoAfter(doAfterEventArgs);
                 }
             };
             args.Verbs.Add(ejectVerb);
