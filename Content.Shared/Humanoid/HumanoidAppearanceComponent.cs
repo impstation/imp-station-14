93fbd10a26c40e412cfed7c2ab8882ddaf956b36
mousesweater <moondogg228@gmail.com>
2025-04-06 12:48:32 -0500

    attribution moment

--- Content.Shared/Humanoid/HumanoidAppearanceComponent.cs
+++ Content.Shared/Humanoid/HumanoidAppearanceComponent.cs
@@ -88,7 +88,7 @@ public sealed partial class HumanoidAppearanceComponent : Component
     ///     Which layers of this humanoid that should be hidden on equipping a corresponding item..
     /// </summary>
     [DataField]
-    public HashSet<HumanoidVisualLayers> HideLayersOnEquip = [HumanoidVisualLayers.Hair];
+    public HashSet<HumanoidVisualLayers> HideLayersOnEquip = [HumanoidVisualLayers.Hair, HumanoidVisualLayers.FacialHair]; /// IMP EDIT: FACIAL HAIR SHAVE
 
     /// <summary>
     ///     Which markings the humanoid defaults to when nudity is toggled off.
