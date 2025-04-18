799a02d9895130638a9ee04b7c764c45ffbcd04e
Asa <mishka.chan@gmail.com>
2025-04-17 20:00:39 -0700

    who am i. where am i.

---
+++ Content.Shared/_Impstation/Radio/SelfHeadsetSystem.cs
@@ -0,0 +1,101 @@
+﻿using Robust.Shared.Containers;
+using Content.Shared.Radio;
+using Content.Shared.Radio.Components;
+using Content.Shared.Radio.EntitySystems;
+using Content.Shared.Implants;
+using Content.Shared.Implants.Components;
+
+namespace Content.Server.Impstation.Radio;
+
+public sealed class SelfHeadsetSystem : EntitySystem
+{
+    [Dependency] private readonly RadioImplantComponent _radioChannels = default!;
+    [Dependency] private readonly EncryptionKeyHolderComponent _keysUnlocked = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<SelfHeadsetComponent, EntGotInsertedIntoContainerMessage>(OnAdd);
+        SubscribeLocalEvent<SelfHeadsetComponent, EntGotRemovedFromContainerMessage>(OnRemove);
+    }
+
+
+
+    /// <summary>
+    /// If an encryption key is added, installs the necessary intrinsic radio components
+    /// </summary>
+    private void OnAdd(Entity<SelfHeadsetComponent> ent, ref EncryptionKeyHolderComponent args)
+    {
+
+        var activeRadio = EnsureComp<ActiveRadioComponent>(args.Channels);
+        foreach (var channel in ent.Comp.RadioChannels)
+        {
+            if (activeRadio.Channels.Add(channel))
+                ent.Comp.ActiveAddedChannels.Add(channel);
+        }
+
+        EnsureComp<IntrinsicRadioReceiverComponent>(args.Channels);
+
+        var intrinsicRadioTransmitter = EnsureComp<IntrinsicRadioTransmitterComponent>(args.Channels);
+        foreach (var channel in ent.Comp.RadioChannels)
+        {
+            if (intrinsicRadioTransmitter.Channels.Add(channel))
+                ent.Comp.TransmitterAddedChannels.Add(channel);
+        }
+    }
+
+    public void UpdateChannels(Entity<SelfHeadsetComponent> uid, EncryptionKeyHolderComponent component)
+    {
+        if (!component.Initialized)
+            return;
+
+        component.Channels.Clear();
+        component.DefaultChannel = null;
+
+        foreach (var ent in component.KeyContainer.ContainedEntities)
+        {
+            if (TryComp<EncryptionKeyComponent>(ent, out var key))
+            {
+                component.Channels.UnionWith(key.Channels);
+                component.DefaultChannel ??= key.DefaultChannel;
+            }
+        }
+
+        RaiseLocalEvent(uid, new EncryptionChannelsChangedEvent(component));
+    }
+
+    /// <summary>
+    /// Removes intrinsic radio components once the encryption key is removed
+    /// </summary>
+    private void OnRemove(Entity<SelfHeadsetComponent> ent, ref EntGotRemovedFromContainerMessage args)
+    {
+        if (TryComp<ActiveRadioComponent>(args.Container.Owner, out var activeRadioComponent))
+        {
+            foreach (var channel in ent.Comp.ActiveAddedChannels)
+            {
+                activeRadioComponent.Channels.Remove(channel);
+            }
+            ent.Comp.ActiveAddedChannels.Clear();
+
+            if (activeRadioComponent.Channels.Count == 0)
+            {
+                RemCompDeferred<ActiveRadioComponent>(args.Container.Owner);
+            }
+        }
+
+        if (!TryComp<IntrinsicRadioTransmitterComponent>(args.Container.Owner, out var radioTransmitterComponent))
+            return;
+
+        foreach (var channel in ent.Comp.TransmitterAddedChannels)
+        {
+            radioTransmitterComponent.Channels.Remove(channel);
+        }
+        ent.Comp.TransmitterAddedChannels.Clear();
+
+        if (radioTransmitterComponent.Channels.Count == 0 || activeRadioComponent?.Channels.Count == 0)
+        {
+            RemCompDeferred<IntrinsicRadioTransmitterComponent>(args.Container.Owner);
+        }
+    }
+}
