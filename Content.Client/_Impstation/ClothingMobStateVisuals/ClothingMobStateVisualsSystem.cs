ad38f41ac9fec254ab8fdc0ccd607795db7e47ca
widgetbeck <beckparrott@gmail.com>
2025-04-18 00:01:00 -0400

    griffy suit systems are done

--- Content.Client/_Impstation/ClothingMobStateVisuals/ClothingMobStateVisualsSystem.cs
+++ Content.Client/_Impstation/ClothingMobStateVisuals/ClothingMobStateVisualsSystem.cs
@@ -1,58 +1,21 @@
 using Content.Shared._Impstation.ClothingMobStateVisuals;
-using Content.Shared.Humanoid.Prototypes;
-using Content.Shared.Mobs.Systems;
-using Content.Shared.Inventory;
-using Robust.Client.GameObjects;
-using Robust.Shared.Prototypes;
-using Content.Shared.Clothing;
 using Content.Shared.Item;
-using System.Linq;
-using Content.Shared.Clothing.Components;
-using Content.Shared.Clothing.EntitySystems;
 
 namespace Content.Client._Impstation.ClothingMobStateVisuals;
 
 public sealed partial class ClothingMobStateVisualsSystem : SharedClothingMobStateVisualsSystem
 {
-    [Dependency] private readonly IPrototypeManager _protoMan = default!;
-    [Dependency] private readonly MobStateSystem _mobState = default!;
     [Dependency] private readonly SharedItemSystem _itemSys = default!;
-    [Dependency] private readonly ClothingSystem _clothing = default!;
 
     public override void Initialize()
     {
         base.Initialize();
 
         SubscribeLocalEvent<ClothingMobStateVisualsComponent, ClothingMobStateChangedEvent>(OnClothingMobStateChanged);
-        SubscribeLocalEvent<ClothingMobStateVisualsComponent, GetEquipmentVisualsEvent>(OnGetEquipmentVisuals);
     }
-
-
 
     private void OnClothingMobStateChanged(Entity<ClothingMobStateVisualsComponent> ent, ref ClothingMobStateChangedEvent args)
     {
-        if (!_protoMan.TryIndex(args.SpeciesId, out _))
-            return;
-        if (!TryComp<SpriteComponent>(ent, out var sprite))
-            return;
-
-        var enabled = _mobState.IsIncapacitated(args.Equippee);
-        if (sprite != null && sprite.LayerMapTryGet(ent.Comp.SpriteLayer, out var layer))
-        {
-            sprite.LayerSetVisible(layer, enabled);
-        }
         _itemSys.VisualsChanged(ent);
-    }
-
-    private void OnGetEquipmentVisuals(Entity<ClothingMobStateVisualsComponent> ent, ref GetEquipmentVisualsEvent args)
-    {
-        if (!TryComp(args.Equipee, out InventoryComponent? inventory) || !TryComp(ent, out ClothingComponent? clothingComp))
-            return;
-
-        if (ent.Comp.ClothingPrefix == null)
-            ent.Comp.ClothingPrefix = clothingComp.EquippedPrefix;
-
-        var prefix = _mobState.IsIncapacitated(args.Equipee) ? ent.Comp.IncapacitatedPrefix : ent.Comp.ClothingPrefix;
-        _clothing.SetEquippedPrefix(ent, prefix, clothingComp);
     }
 }
--- Content.Shared/_Impstation/ClothingMobStateVisuals/ClothingMobStateVisualsComponent.cs
+++ Content.Shared/_Impstation/ClothingMobStateVisuals/ClothingMobStateVisualsComponent.cs
@@ -1,21 +1,10 @@
-using Content.Shared.Humanoid.Prototypes;
-using Content.Shared.Mobs;
 using Robust.Shared.GameStates;
-using Robust.Shared.Prototypes;
-using Robust.Shared.Serialization;
-using Robust.Shared.Toolshed.TypeParsers;
 
 namespace Content.Shared._Impstation.ClothingMobStateVisuals;
 
 [RegisterComponent, NetworkedComponent]
 public sealed partial class ClothingMobStateVisualsComponent : Component
 {
-    [DataField]
-    public string SpriteLayer = "incapacitated";
-
-    [DataField]
-    public Dictionary<string, List<PrototypeLayerData>> ClothingVisuals = new();
-
     [DataField]
     public string IncapacitatedPrefix = "incapacitated";
 
@@ -24,16 +13,8 @@ public sealed partial class ClothingMobStateVisualsComponent : Component
 
 public sealed class ClothingMobStateChangedEvent : EntityEventArgs
 {
-    public ClothingMobStateChangedEvent(Entity<ClothingMobStateVisualsComponent> ent, EntityUid equippee, ProtoId<SpeciesPrototype> species, MobState newMobState)
+    public ClothingMobStateChangedEvent()
     {
-        Ent = ent;
-        Equippee = equippee;
-        SpeciesId = species;
-        NewMobState = newMobState;
-    }
 
-    public Entity<ClothingMobStateVisualsComponent> Ent { get; }
-    public EntityUid Equippee { get; }
-    public ProtoId<SpeciesPrototype> SpeciesId { get; }
-    public MobState NewMobState { get; }
+    }
 }
