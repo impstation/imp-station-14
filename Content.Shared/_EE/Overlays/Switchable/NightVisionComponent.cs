61e77674f5043e675cb313966c3ba5768391f2d8
widgetbeck <beckparrott@gmail.com>
2025-04-25 07:46:23 -0400

    drones get special good night vision

--- Content.Shared/_EE/Overlays/Switchable/NightVisionComponent.cs
+++ Content.Shared/_EE/Overlays/Switchable/NightVisionComponent.cs
@@ -9,6 +9,12 @@ public sealed partial class NightVisionComponent : SwitchableOverlayComponent
     public override string? ToggleAction { get; set; } = "ToggleNightVision";
 
     public override Color Color { get; set; } = Color.FromHex("#98FB98");
+
+    /// <summary>
+    /// imp. if true, uses DrawLighting instead of DrawShadows. Unfortunately this makes aliens naked, so I can only use it on drones.
+    /// </summary>
+    [DataField]
+    public bool UseGoodLighting;
 }
 
 public sealed partial class ToggleNightVisionEvent : InstantActionEvent;
