c7dbabbeecb7ea54de9a029a19edad094e6a634a
DVDPlayerOfDiscordFame <drew.c.bergen@gmail.com>
2025-05-01 14:19:18 -0700

    I can't test this yet please help

---
+++ Content.Shared/_Impstation/Weapons/Melee/Components/OurborosMarkerComponent.cs
@@ -0,0 +1,7 @@
+/// <summary>
+/// An empty marker comp that marks an entity to be passively repelled by the Ouroboros gun.
+/// </summary>
+[RegisterComponent]
+public sealed partial class yourMarkerComponent : OuroborosMarker
+{
+}
