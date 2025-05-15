e1449733f673aba2f133d6e32bd50d7d62acc51a
TheLuggage05 <beard.maclean@gmail.com>
2025-05-14 19:21:14 -0700

    all of it

---
+++ Content.Server/_Impstation/Captain/Components/CaptainSwordMenuComponent.cs
@@ -0,0 +1,33 @@
+using Content.Server._Impstation.Captain.Systems;
+using Content.Shared._Impstation.Captain;
+using Content.Shared.Damage;
+using Robust.Shared.Audio;
+using Robust.Shared.Prototypes;
+
+namespace Content.Server._Impstation.Captain.Components;
+
+/// <summary>
+/// This component stores the possible contents of the backpack,
+/// which can be selected via the interface.
+/// </summary>
+[RegisterComponent, Access(typeof(CaptainSwordMenuSystem))]
+public sealed partial class CaptainSwordMenuComponent : Component
+{
+    /// <summary>
+    /// List of sets available for selection
+    /// </summary>
+    [DataField]
+    public List<ProtoId<CaptainSwordMenuSetPrototype>> PossibleSets = new();
+
+    [DataField]
+    public List<int> SelectedSets = new();
+
+    [DataField]
+    public SoundSpecifier ApproveSound = new SoundPathSpecifier("/Audio/Effects/holy.ogg");
+
+    /// <summary>
+    /// Max number of sets you can select.
+    /// </summary>
+    [DataField]
+    public int MaxSelectedSets = 1;
+}
---
+++ Resources/Locale/en-US/_Impstation/captain/gearmenu.ftl
@@ -0,0 +1,17 @@
+captain-sheathe-transformed = Your liquid metal blade coalesces!
+
+captain-swordmenu-window-title = Form Blade
+captain-swordmenu-window-description =
+    Permanently shape your sword into a shape of your choice.
+captain-swordmenu-window-selected = Form selected: ({$selectedCount}/{$maxCount})
+
+captain-swordset-category-sabre-name = Captain's Sabre
+captain-swordset-category-sabre-description =
+    A classic.
+    Heavy slash damage, and fast attack rate make it extremely
+    reliable.
+
+captain-swordset-category-rapier-name = Captain's Rapier
+captain-swordset-category-rapier-description =
+    For the more cunning individual.
+    Trades off damage and wide swings to pierce armor perfectly.
