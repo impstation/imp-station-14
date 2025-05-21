e31c58fe1d06ea01220404a8ea33b17b8856141d
mqole <anactualpanacea@gmail.com>
2025-05-21 13:04:45 +1000

    yay!

---
+++ Content.Server/_Impstation/Speech/EntitySystems/PGAccentSystem.cs
@@ -0,0 +1,24 @@
+using System.Text.RegularExpressions;
+using Content.Server.Speech.Components;
+
+namespace Content.Server.Speech.EntitySystems;
+
+public sealed class PGAccentSystem : EntitySystem
+{
+    [Dependency] private readonly ReplacementAccentSystem _replacement = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<PGAccentComponent, AccentGetEvent>(OnAccent);
+    }
+
+    private void OnAccent(EntityUid uid, PGAccentComponent component, AccentGetEvent args)
+    {
+        var message = args.Message;
+
+        message = _replacement.ApplyReplacements(message, "pg_accent");
+
+        args.Message = message;
+    }
+}
