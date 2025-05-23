7296a548b2c2a9ce5dd304dcfe2f2f9cc968daf5
widgetbeck <beckparrott@gmail.com>
2025-05-22 23:11:04 -0400

    cargorilla combat mode visuals + sprint, also chest beat emote & rare roundstart cargorilla chance

---
+++ Content.Server/_Impstation/CombatModeSprint/CombatModeSprintSystem.cs
@@ -0,0 +1,11 @@
+using Content.Server.CombatMode;
+using Content.Shared._Impstation.CombatModeSprint;
+using Content.Shared.CombatMode;
+using Content.Shared.Movement.Systems;
+
+namespace Content.Server._Impstation.CombatModeSprint;
+
+public sealed partial class CombatModeSprintSystem : SharedCombatModeSprintSystem
+{
+
+}
