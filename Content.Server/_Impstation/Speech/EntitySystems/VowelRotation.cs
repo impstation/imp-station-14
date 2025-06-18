189bc006ac33c92f8da0eb46223e422609b3e4a7
widgetbeck <beckparrott@gmail.com>
2025-06-18 17:09:58 -0400

    significantly improve the vowel rotation accent + fix EIOUE + fix whitespace

--- Content.Server/_Impstation/Speech/EntitySystems/VowelRotation.cs
+++ Content.Server/_Impstation/Speech/EntitySystems/VowelRotation.cs
@@ -1,22 +1,10 @@
-using System.Linq;
-using System.Text.RegularExpressions;
+using System.Text;
 using Content.Server.Speech.Components;
 
 namespace Content.Server.Speech.EntitySystems;
 
 public sealed partial class VowelRotationSystem : EntitySystem
 {
-    private static readonly Regex RegexLowerA = new("a");
-    private static readonly Regex RegexLowerE = new("e");
-    private static readonly Regex RegexLowerI = new("i");
-    private static readonly Regex RegexLowerO = new("o");
-    private static readonly Regex RegexLowerU = new("u");
-    private static readonly Regex RegexUpperA = new("A");
-    private static readonly Regex RegexUpperE = new("E");
-    private static readonly Regex RegexUpperI = new("I");
-    private static readonly Regex RegexUpperO = new("O");
-    private static readonly Regex RegexUpperU = new("U");
-
     public override void Initialize()
     {
         base.Initialize();
@@ -27,18 +15,25 @@ public sealed partial class VowelRotationSystem : EntitySystem
     {
         var message = args.Message;
 
-        message = RegexLowerU.Replace(message, "a");
-        message = RegexLowerO.Replace(message, "u");
-        message = RegexLowerI.Replace(message, "o");
-        message = RegexLowerE.Replace(message, "i");
-        message = RegexLowerA.Replace(message, "e");
+        var msgBuilder = new StringBuilder(message);
+        for (var i = 0; i < msgBuilder.Length; i++)
+        {
+            msgBuilder[i] = msgBuilder[i] switch
+            {
+                'a' => 'e',
+                'e' => 'i',
+                'i' => 'o',
+                'o' => 'u',
+                'u' => 'a',
+                'A' => 'E',
+                'E' => 'I',
+                'I' => 'O',
+                'O' => 'U',
+                'U' => 'A',
+                _ => msgBuilder[i]
+            };
+        }
 
-        message = RegexUpperU.Replace(message, "A");
-        message = RegexUpperO.Replace(message, "U");
-        message = RegexUpperI.Replace(message, "O");
-        message = RegexUpperE.Replace(message, "I");
-        message = RegexUpperA.Replace(message, "E");
-
-        args.Message = message;
+        args.Message = msgBuilder.ToString();
     }
 }
