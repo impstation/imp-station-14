3a4763a339e0adc476250f535241317755356087
sev7ves <soapedzone@gmail.com>
2025-06-27 14:10:56 -0400

    okay no longer instant, but i made it really fast because thats better

--- Content.Server/_Impstation/Pleebnar/PleebnarGibSystem.cs
+++ Content.Server/_Impstation/Pleebnar/PleebnarGibSystem.cs
@@ -4,9 +4,7 @@ using Content.Shared._Impstation.Pleebnar;
 using Content.Shared._Impstation.Pleebnar.Components;
 using Content.Shared.Body.Components;
 using Content.Shared.DoAfter;
-using Content.Shared.Gibbing.Systems;
 using Content.Shared.Popups;
-using Robust.Shared.Physics.Components;
 
 namespace Content.Server._Impstation.Pleebnar;
 /// <summary>
@@ -32,11 +30,9 @@ public sealed class PleebnarGibSystem : SharedPleebnarGibSystem
         {
             return;
         }
-        if (!TryComp<PhysicsComponent>(args.Target, out var targetPhysics))//try to get physics componenet, if not present return
-            return;
 
         _popupSystem.PopupEntity(Loc.GetString("pleebnar-focus"),ent.Owner,PopupType.Small);
-        var doargs = new DoAfterArgs(EntityManager, ent, targetPhysics.Mass/10, new SharedPleebnarGibSystem.PleebnarGibDoAfterEvent(), ent, args.Target)
+        var doargs = new DoAfterArgs(EntityManager, ent, 0.5f, new SharedPleebnarGibSystem.PleebnarGibDoAfterEvent(), ent, args.Target)
         {
             DistanceThreshold = 15f,
             BreakOnDamage = true,
@@ -44,14 +40,14 @@ public sealed class PleebnarGibSystem : SharedPleebnarGibSystem
             BreakOnMove = true,
             BreakOnWeightlessMove = true,
             AttemptFrequency = AttemptFrequency.StartAndEnd
-        };//lifted from baby kode code, basically takes longer to gib the more mass there is,
+        };
         _doAfter.TryStartDoAfter(doargs);
         args.Handled = true;
     }
     //actually handle the gibbing
     public void PleebnarGibDoafter(Entity<PleebnarGibActionComponent> ent, ref PleebnarGibDoAfterEvent args)
     {
-        if (args.Target == null)//if the target somehow dissapears then return
+        if (args.Target == null||args.Cancelled )//if the target somehow dissapears or the action was cancelled then return
         {
             return;
         }
