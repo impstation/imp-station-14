3cdc2d2e2d3e46d0395d3aba9be4941d70dc13be
TheLuggage05 <beard.maclean@gmail.com>
2025-07-07 02:32:25 -0700

    i think that's the component good

--- Content.Shared/_Impstation/Arcfiend/ArcfiendComponent.cs
+++ Content.Shared/_Impstation/Arcfiend/ArcfiendComponent.cs
@@ -7,5 +7,33 @@ using Robust.Shared.Prototypes;
 namespace Content.Shared.Arcfiend;
 
 [RegisterComponent, NetworkedComponent]
+[AutoGenerateComponentState]
 public sealed partial class ArcfiendComponent : Component
-{}
+{
+    #region Prototypes
+
+    public readonly List<EntProtoId> BaseArcfiendActions = new()
+    {
+        "ActionSapPower",
+        "ActionDischarge",
+        "ActionFlash",
+        "ActionArcFlash",
+        "ActionRideTheLightning",
+        "ActionJammingField",
+        "ActionJolt"
+    };
+
+    #endregion
+
+    /// <summary>
+    ///    Amount of energy the arcfiend has.
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public float Energy = 0f;
+
+    /// <summary>
+    ///     Maximum amount of energy the arcfiend can have.
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public float MaxEnergy = 2500f;
+}
