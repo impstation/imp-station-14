7606c0c97db21132d1a696a903e2df1f2c12d796
iaada <iaada@users.noreply.github.com>
2025-07-07 21:05:30 -0500

    initial functional

---
+++ Content.Shared/ItemToggleDoAfterComponent.cs
@@ -0,0 +1,11 @@
+namespace Content.Shared.Item.ItemToggle.Components;
+
+/// <summary>
+/// This is used for...
+/// </summary>
+[RegisterComponent]
+public sealed partial class ItemToggleDoAfterComponent : Component
+{
+    [DataField]
+    public float DoAfterTime = .75f;
+}
