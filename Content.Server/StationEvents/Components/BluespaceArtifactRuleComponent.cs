0b838e5bb73c97ad03d16a22606b95e90c31e298
TheGrimbeeper <thegrimbeeper.11@gmail.com>
2025-07-22 19:50:06 +1000

    imp comments

--- Content.Server/StationEvents/Components/BluespaceArtifactRuleComponent.cs
+++ Content.Server/StationEvents/Components/BluespaceArtifactRuleComponent.cs
@@ -12,7 +12,7 @@ namespace Content.Server.StationEvents.Components;
 public sealed partial class BluespaceArtifactRuleComponent : Component
 {
     [DataField("artifactSpawnerPrototype", customTypeSerializer: typeof(PrototypeIdSerializer<EntityPrototype>))]
-    public string ArtifactSpawnerPrototype = "RandomOldNewArtifactSpawner";
+    public string ArtifactSpawnerPrototype = "RandomOldNewArtifactSpawner"; //#IMP RandomOldNewArtifactSpawner from RandomArtifactSpawner
 
     [DataField("artifactFlashPrototype", customTypeSerializer: typeof(PrototypeIdSerializer<EntityPrototype>))]
     public string ArtifactFlashPrototype = "EffectFlashBluespace";
