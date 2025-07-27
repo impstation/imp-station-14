5f598f6ddb77740561ccee14160477a9200509a4
TheGrimbeeper <thegrimbeeper.11@gmail.com>
2025-07-27 20:49:14 +1000

    fix natural artifact suppression

---
+++ Content.Server/Xenoarchaeology/Equipment/SuppressArtifactContainerComponent.cs
@@ -0,0 +1,11 @@
+﻿using Robust.Shared.GameStates;
+using Content.Server.Xenoarchaeology.Equipment.Systems;
+
+namespace Content.Server.Xenoarchaeology.Equipment.Components;
+//#IMP: Moved from Shared to Server for reason of allowing it to access old Xenoarch.
+
+/// <summary>
+///     Suppress artifact activation, when entity is placed inside this container.
+/// </summary>
+[RegisterComponent] //#IMP can't have network as this isn't in shared
+public sealed partial class SuppressArtifactContainerComponent : Component;
---
+++ Content.Server/Xenoarchaeology/Equipment/Systems/SuppressArtifactContainerSystem.cs
@@ -0,0 +1,37 @@
+﻿using Content.Server.Xenoarchaeology.XenoArtifacts; //#IMP
+using Content.Server.Xenoarchaeology.Equipment.Components; //#IMP
+using Content.Shared.Xenoarchaeology.Artifact;
+using Content.Shared.Xenoarchaeology.Artifact.Components;
+using Robust.Shared.Containers;
+
+namespace Content.Server.Xenoarchaeology.Equipment.Systems;
+
+//#IMP: Moved from Shared to Server for reason of allowing it to access old Xenoarch.
+public sealed class SuppressArtifactContainerSystem : EntitySystem
+{
+    [Dependency] private readonly SharedXenoArtifactSystem _xenoArtifact = default!;
+    [Dependency] private readonly ArtifactSystem _artifact = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<SuppressArtifactContainerComponent, EntInsertedIntoContainerMessage>(OnInserted);
+        SubscribeLocalEvent<SuppressArtifactContainerComponent, EntRemovedFromContainerMessage>(OnRemoved);
+    }
+
+    private void OnInserted(EntityUid uid, SuppressArtifactContainerComponent component, EntInsertedIntoContainerMessage args)
+    {
+        if (TryComp<XenoArtifactComponent>(args.Entity, out var artifact)) //#IMP changed this from if (!trycomp)
+            _xenoArtifact.SetSuppressed((args.Entity, artifact), true);
+        if (TryComp<ArtifactComponent>(args.Entity, out var oldArtifact)) //#IMP
+            _artifact.SetIsSuppressed(args.Entity, true, oldArtifact);
+    }
+
+    private void OnRemoved(EntityUid uid, SuppressArtifactContainerComponent component, EntRemovedFromContainerMessage args)
+    {
+        if (TryComp<XenoArtifactComponent>(args.Entity, out var artifact)) //#IMP changed this from if (!trycomp)
+            _xenoArtifact.SetSuppressed((args.Entity, artifact), false);
+        if (TryComp<ArtifactComponent>(args.Entity, out var oldArtifact)) //#IMP
+            _artifact.SetIsSuppressed(args.Entity, false, oldArtifact);
+    }
+}
