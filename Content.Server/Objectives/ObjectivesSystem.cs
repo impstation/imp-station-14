49d1bb5d165f5ad5ae514d67542ba97539dc0401
AirFryerBuyOneGetOneFree <jakoblondon01@gmail.com>
2025-08-27 21:52:51 -0500

    Non traitors get names in the round end again

--- Content.Server/Objectives/ObjectivesSystem.cs
+++ Content.Server/Objectives/ObjectivesSystem.cs
@@ -146,10 +146,19 @@ public sealed class ObjectivesSystem : SharedObjectivesSystem
             var custody = IsInCustody(mindId, mind) ? Loc.GetString("objectives-in-custody") : string.Empty;
             var objectiveIssuer = mind.ObjectiveIssuer; // Imp Edit traitor flavor end screen
             var objectives = mind.Objectives;
-            if (objectives.Count == 0)
+            var agentPrepped = "";
+            if (objectiveIssuer != null) // Imp Edit traitor flavor end screen
+            {
+                agentPrepped = Loc.GetString($"traitor-{objectiveIssuer}-roundend");
+            }
+            else
+            {
+                agentPrepped = agent;
+            }
+            if (objectives.Count == 0 && agentPrepped != null)
             {
                 agentSummaries.Add((
-                    Loc.GetString("objectives-no-objectives", ("custody", custody), ("title", title), ("agent", Loc.GetString($"traitor-{objectiveIssuer}-roundend"))),// Imp Edit traitor flavor end screen
+                    Loc.GetString("objectives-no-objectives", ("custody", custody), ("title", title), ("agent", agentPrepped)),// Imp Edit traitor flavor end screen
                     0f, 0));
                 continue;
             }
@@ -165,7 +174,7 @@ public sealed class ObjectivesSystem : SharedObjectivesSystem
             agentSummary.AppendLine(Loc.GetString("objectives-with-objectives",
                 ("custody", custody),
                 ("title", title),
-                ("agent", Loc.GetString($"traitor-{objectiveIssuer}-roundend"))));// Imp Edit traitor flavor end screen
+                ("agent", agentPrepped)));// Imp Edit traitor flavor end screen
 
             foreach (var objectiveGroup in objectives.GroupBy(o => Comp<ObjectiveComponent>(o).LocIssuer))
             {
