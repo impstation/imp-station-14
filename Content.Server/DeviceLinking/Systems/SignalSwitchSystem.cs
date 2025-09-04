bde8d59dcd0f0826f63436157962288f9085d795
PaigeM~ <roseliasquartz@gmail.com>
2025-09-04 00:19:52 -0700

    integrate that shit

--- Content.Server/DeviceLinking/Systems/SignalSwitchSystem.cs
+++ Content.Server/DeviceLinking/Systems/SignalSwitchSystem.cs
@@ -43,17 +43,8 @@ public sealed class SignalSwitchSystem : EntitySystem
             _deviceLink.SendSignal(uid, comp.StatusPort, comp.State);
         }
 
-        var ev = new SwitchPressedEvent(); //imp
-        RaiseLocalEvent(uid, ref ev); //imp
-
         _audio.PlayPvs(comp.ClickSound, uid, AudioParams.Default.WithVariation(0.125f).WithVolume(8f));
 
         args.Handled = true;
     }
 }
-
-[ByRefEvent]
-/// <summary>
-/// IMP. Event to inform any other systems that may want to know that the signal switch was pressed
-/// </summary>
-public record struct SwitchPressedEvent();
