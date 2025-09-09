ba05a817e121a36b0e65e848ca45eb8d96b5cfbc
Alex C <alex91905@yahoo.com>
2025-09-09 02:36:28 -0400

    help

--- Content.Server/_Goobstation/Heretic/EntitySystems/MinionSystem.cs
+++ Content.Server/_Goobstation/Heretic/EntitySystems/MinionSystem.cs
@@ -25,51 +25,51 @@ public sealed class MinionSystem : EntitySystem
     [Dependency] private readonly SharedMindSystem _mind = default!;
     [Dependency] private readonly SharedRoleSystem _role = default!;
 
-    public void ConvertEntityToMinion(EntityUid minion, MinionComponent comp, bool? createGhostRole, bool? sendBriefing, bool? removeBaseFactions)
+    public void ConvertEntityToMinion(Entity<MinionComponent> ent, bool? createGhostRole, bool? sendBriefing, bool? removeBaseFactions)
     {
-        var hasMind = _mind.TryGetMind(minion, out var mindId, out _);
+        var hasMind = _mind.TryGetMind(ent, out var mindId, out _);
 
         if (hasMind && sendBriefing == true)
         {
-            if (comp.BoundOwner != null)
-                SendBriefing(minion, comp, mindId, comp.BoundOwner.Value);
+            if (ent.Comp.BoundOwner != null)
+                SendBriefing((ent, ent.Comp), mindId, ent.Comp.BoundOwner.Value);
 
             if (_playerManager.TryGetSessionByEntity(mindId, out var session))
                 _euiMan.OpenEui(new GhoulNotifEui(), session);
         }
 
-        _mind.MakeSentient(minion);
+        _mind.MakeSentient(ent);
         _role.MindAddRole(mindId, "MindRoleGhostRoleFamiliar");
 
-        if (!HasComp<GhostRoleComponent>(minion) && !hasMind && createGhostRole == true)
+        if (!HasComp<GhostRoleComponent>(ent) && !hasMind && createGhostRole == true)
         {
-            var ghostRole = EnsureComp<GhostRoleComponent>(minion);
-            ghostRole.RoleName = Loc.GetString(comp.GhostRoleName);
-            ghostRole.RoleDescription = Loc.GetString(comp.GhostRoleDescription);
-            ghostRole.RoleRules = Loc.GetString(comp.GhostRoleRules);
+            var ghostRole = EnsureComp<GhostRoleComponent>(ent);
+            ghostRole.RoleName = Loc.GetString(ent.Comp.GhostRoleName);
+            ghostRole.RoleDescription = Loc.GetString(ent.Comp.GhostRoleDescription);
+            ghostRole.RoleRules = Loc.GetString(ent.Comp.GhostRoleRules);
         }
 
-        if (!HasComp<GhostRoleMobSpawnerComponent>(minion) && !hasMind)
-            EnsureComp<GhostTakeoverAvailableComponent>(minion);
+        if (!HasComp<GhostRoleMobSpawnerComponent>(ent) && !hasMind)
+            EnsureComp<GhostTakeoverAvailableComponent>(ent);
 
         if (removeBaseFactions == true)
-            _faction.ClearFactions((minion, null));
+            _faction.ClearFactions((ent, null));
 
-        foreach (var faction in comp.FactionsToAdd)
+        foreach (var faction in ent.Comp.FactionsToAdd)
         {
-            _faction.AddFaction((minion, null), faction);
+            _faction.AddFaction((ent, null), faction);
         }
     }
 
-    private void SendBriefing(EntityUid minion, MinionComponent comp, EntityUid owner, EntityUid mindId)
+    private void SendBriefing(Entity<MinionComponent> ent, EntityUid owner, EntityUid mindId)
     {
-        var brief = Loc.GetString(comp.Briefing, ("ent", Identity.Entity(owner, EntityManager)));
-        _antag.SendBriefing(minion, brief, Color.MediumPurple, comp.BriefingSound);
+        var brief = Loc.GetString(ent.Comp.Briefing, ("ent", Identity.Entity(owner, EntityManager)));
+        _antag.SendBriefing(ent, brief, Color.MediumPurple, ent.Comp.BriefingSound);
 
-        if (!TryComp<GhoulRoleComponent>(minion, out _))
+        if (!TryComp<GhoulRoleComponent>(ent, out _))
             AddComp(mindId, new GhoulRoleComponent(), overwrite: true);
 
-        if (!TryComp<RoleBriefingComponent>(minion, out var rolebrief))
+        if (!TryComp<RoleBriefingComponent>(ent, out var rolebrief))
             AddComp(mindId, new RoleBriefingComponent { Briefing = brief }, overwrite: true);
         else
             rolebrief.Briefing += $"\n{brief}";
@@ -83,12 +83,15 @@ public sealed class MinionSystem : EntitySystem
         SubscribeLocalEvent<MinionComponent, TakeGhostRoleEvent>(OnTakeGhostRole);
     }
 
-    private void OnTakeGhostRole(EntityUid minion, MinionComponent comp, ref TakeGhostRoleEvent args)
+    private void OnTakeGhostRole(Entity<MinionComponent> ent, ref TakeGhostRoleEvent args)
     {
-        var hasMind = _mind.TryGetMind(minion, out var mindId, out _);
-        if (hasMind && comp.BoundOwner != null)
-            SendBriefing(minion, comp, mindId, comp.BoundOwner.Value);
-        _role.MindAddRole(mindId, "MindRoleGhostRoleFamiliar");
+        var hasMind = _mind.TryGetMind(ent, out var mindId, out var mind);
+
+        if (ent.Comp.BoundOwner == null)
+            return;
+
+        if (hasMind)
+            SendBriefing(ent, ent.Comp.BoundOwner.Value, mindId);
     }
 
     private static void OnTryAttack(Entity<MinionComponent> ent, ref AttackAttemptEvent args)
