2a04796fb580b2bc735c6f33c82fc554dc901d6a
Kip <tfraser520@gmail.com>
2025-09-23 20:48:46 +1000

    accent

---
+++ Content.Server/_Impstation/Speech/Components/AllulaloAccentComponent.cs
@@ -0,0 +1,10 @@
+namespace Content.Server.Speech.Components;
+
+/// <summary>
+///     Scraw!!
+/// </summary>
+[RegisterComponent]
+public sealed partial class AllulaloAccentComponent : Component
+{
+
+}
