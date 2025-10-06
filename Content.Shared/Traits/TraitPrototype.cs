2d4f937843a9fb892e067ec422b02279ae086a6c
mqole <anactualpanacea@gmail.com>
2025-10-06 14:23:08 +1100

    ui shit & nitpicks

--- Content.Shared/Traits/TraitPrototype.cs
+++ Content.Shared/Traits/TraitPrototype.cs
@@ -1,8 +1,8 @@
 using Content.Shared.Tag;
 using Content.Shared.Whitelist;
 using Robust.Shared.Prototypes;
-using Content.Shared.Humanoid.Prototypes; // DeltaV - Trait species hiding
 using Content.Shared._Impstation.Traits; // Imp - Subcategories
+using Content.Shared.Humanoid.Prototypes; // DeltaV - Trait species hiding
 
 namespace Content.Shared.Traits;
 
@@ -80,5 +80,5 @@ public sealed partial class TraitPrototype : IPrototype
     /// Imp - Traits with the same subcategory cannot be taken in tandem for free points (e.g. blindness and colorblindness)
     /// </summary>
     [DataField]
-    public HashSet<ProtoId<TraitSubcategoryPrototype>> Subcategories = new();
+    public HashSet<ProtoId<TraitSubcategoryPrototype>> Subcategories = [];
 }
