01d857b0606d77292f312c8c6e6dede03d48a00c
msTheowo <31812973+msTheowo@users.noreply.github.com>
2025-11-25 15:28:31 -0500

    standing fix

--- Content.Server/_EE/InteractionVerbs/Actions/ChangeStandingStateAction.cs
+++ Content.Server/_EE/InteractionVerbs/Actions/ChangeStandingStateAction.cs
@@ -15,10 +15,10 @@ public sealed partial class ChangeStandingStateAction : InteractionAction
             return false;
 
         if (isBefore)
-            args.Blackboard["standing"] = state.CurrentState;
+            args.Blackboard["standing"] = state.Standing;
 
-        return state.CurrentState == StandingState.Standing && MakeLaying
-               || state.CurrentState == StandingState.Lying && MakeStanding;
+        return state.Standing && MakeLaying
+               || !state.Standing && MakeStanding;
     }
 
     public override bool Perform(InteractionArgs args, InteractionVerbPrototype proto, VerbDependencies deps)
@@ -26,13 +26,13 @@ public sealed partial class ChangeStandingStateAction : InteractionAction
         var stateSystem = deps.EntMan.System<StandingStateSystem>();
 
         if (!deps.EntMan.TryGetComponent<StandingStateComponent>(args.Target, out var state)
-            || args.TryGetBlackboard("standing", out StandingState oldStanding) && oldStanding != state.CurrentState)
+            || args.TryGetBlackboard("standing", out bool standing) && standing != state.Standing)
             return false;
 
         // Note: these will get cancelled if the target is forced to stand/lay, e.g. due to a buckle or stun or something else.
-        if (state.CurrentState == StandingState.Lying && MakeStanding)
+        if (!state.Standing && MakeStanding)
             return stateSystem.Stand(args.Target);
-        else if (state.CurrentState == StandingState.Standing && MakeLaying)
+        else if (state.Standing && MakeLaying)
             return stateSystem.Down(args.Target);
 
         return false;
