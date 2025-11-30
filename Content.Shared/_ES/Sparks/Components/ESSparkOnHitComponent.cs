3a2e3f328aebe212b14b46fcbdedb3d90023d237
Nemanja <98561806+EmoGarbage404@users.noreply.github.com>
2025-11-29 23:33:33 -0500

    Spark tweaks (#330)
    (cherry picked from commit 8b22cb9b5005db10eb9bac944f3fd6898e4afb2d)

--- Content.Shared/_ES/Sparks/Components/ESSparkOnHitComponent.cs
+++ Content.Shared/_ES/Sparks/Components/ESSparkOnHitComponent.cs
@@ -1,13 +1,14 @@
 using Content.Shared.FixedPoint;
 using Robust.Shared.GameStates;
 using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
 
 namespace Content.Shared._ES.Sparks.Components;
 
 /// <summary>
 /// An entity that sparks when damaged by something
 /// </summary>
-[RegisterComponent, NetworkedComponent]
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentPause]
 [Access(typeof(ESSparkOnHitSystem))]
 public sealed partial class ESSparkOnHitComponent : Component
 {
@@ -22,6 +23,25 @@ public sealed partial class ESSparkOnHitComponent : Component
     /// </summary>
     [DataField]
     public int Count = 3;
+
+    /// <summary>
+    /// Chance for sparks to occur
+    /// </summary>
+    [DataField]
+    public float Prob = 1f;
+
+    /// <summary>
+    /// Minimum time inbetween sparks occuring from hits.
+    /// Used to reduce spark spam.
+    /// </summary>
+    [DataField]
+    public TimeSpan SparkDelay = TimeSpan.FromSeconds(0.5f);
+
+    /// <summary>
+    /// The last time that a spark occured.
+    /// </summary>
+    [DataField(customTypeSerializer: typeof(TimeOffsetSerializer)), AutoPausedField]
+    public TimeSpan LastSparkTime;
 
     /// <summary>
     /// Spark prototypes
--- Content.Shared/_ES/Sparks/ESSparkOnHitSystem.cs
+++ Content.Shared/_ES/Sparks/ESSparkOnHitSystem.cs
@@ -1,10 +1,14 @@
 using Content.Shared._ES.Sparks.Components;
 using Content.Shared.Damage.Systems;
+using Robust.Shared.Random;
+using Robust.Shared.Timing;
 
 namespace Content.Shared._ES.Sparks;
 
 public sealed class ESSparkOnHitSystem : EntitySystem
 {
+    [Dependency] private readonly IGameTiming _timing = default!;
+    [Dependency] private readonly IRobustRandom _random = default!;
     [Dependency] private readonly ESSparksSystem _sparks = default!;
 
     /// <inheritdoc/>
@@ -18,9 +22,16 @@ public sealed class ESSparkOnHitSystem : EntitySystem
         if (args.DamageDelta is null)
             return;
 
+        if (!_random.Prob(ent.Comp.Prob))
+            return;
+
         if (args.DamageDelta.GetTotal() < ent.Comp.Threshold)
             return;
 
+        if (_timing.CurTime - ent.Comp.LastSparkTime < ent.Comp.SparkDelay)
+            return;
+
         _sparks.DoSparks(ent, ent.Comp.Count, ent.Comp.SparkPrototype);
+        ent.Comp.LastSparkTime = _timing.CurTime;
     }
 }
