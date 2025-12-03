ca74c65f5913b50911d6298941d18376644ea1e6
AvianMaiden <188556051+AvianMaiden@users.noreply.github.com>
2025-12-03 12:22:39 -0800

    it's that shrimple

--- Content.Shared/_Coyote/RadioNoises/RadioStaticComponent.cs
+++ Content.Shared/_Coyote/RadioNoises/RadioStaticComponent.cs
@@ -44,7 +44,7 @@ public sealed partial class RadioStaticComponent : Component
     /// If this is true, the radio will not play any static noise at all.
     /// </summary>
     [ViewVariables(VVAccess.ReadWrite)]
-    public bool OmniSquelch = false;
+    public bool OmniSquelch = true;
 
     /// <summary>
     /// How loud the volume be!
