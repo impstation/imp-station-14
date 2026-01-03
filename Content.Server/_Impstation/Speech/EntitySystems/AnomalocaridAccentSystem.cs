176df7a99fed6d3c6a2ce2bbda27602f17f84d43
ATDoop <1959416+ATDoop@users.noreply.github.com>
2026-01-03 10:31:40 -0500

    baba

--- Content.Server/_Impstation/Speech/EntitySystems/AnomalocaridAccentSystem.cs
+++ Content.Server/_Impstation/Speech/EntitySystems/AnomalocaridAccentSystem.cs
@@ -5,23 +5,25 @@ using Content.Shared.Speech;
 
 namespace Content.Server._Impstation.Speech.EntitySystems;
 
-public sealed class AnomalocaridAccentSystem : EntitySystem
+public sealed partial class AnomalocaridAccentSystem : EntitySystem
 {
     [Dependency] private readonly ReplacementAccentSystem _replacement = default!;
-    private static readonly Regex RegexLowerbubbleBL = new Regex("bl{1,3}");
-    private static readonly Regex RegexUpperbubbleBL = new Regex("BL{1,3}");
-    private static readonly Regex RegexCasebubbleBL = new Regex("Bl{1,3}");
-    private static readonly Regex RegexCamelbubbleBL = new Regex("bL{1,3}");
-    private static readonly Regex RegexLowerbubbleGL = new Regex("gl{1,3}");
-    private static readonly Regex RegexUpperbubbleGL = new Regex("GL{1,3}");
-    private static readonly Regex RegexCasebubbleGL = new Regex("Gl{1,3}");
-    private static readonly Regex RegexCamelbubbleGL = new Regex("gL{1,3}");
-    private static readonly Regex RegexLowerdoubleKK = new Regex("(?<!c)k");
-    private static readonly Regex RegexUpperdoubleKK = new Regex("(?<!C)K}");
-    private static readonly Regex RegexLowerrepeatkCK = new Regex("ck{1,3}");
-    private static readonly Regex RegexUpperrepeatkCK = new Regex("CK{1,3}");
-    private static readonly Regex RegexCaserepeatkCK = new Regex("Ck{1,3}");
-    private static readonly Regex RegexCamelrepeatkCK = new Regex("cK{1,3}");
+
+    private static readonly Dictionary<Regex, string> Regexes = new()
+    {
+        {new ("bl"),"blblbl"},
+        {new ("Bl"),"Blblbl"},
+        {new ("BL"),"BLBLBL"},
+        {new ("gl"),"glglgl"},
+        {new ("Gl"),"Glglgl"},
+        {new ("GL"),"GLGGLL"},
+        {new ("(?<!c)k"),"k-k"},
+        {new ("(?<!C)K"),"K-K"},
+        {new ("ck"),"ck-k"},
+        {new ("CK"),"CK-K"},
+        {new ("Ck"),"Ck-k"},
+        {new ("cK"),"cK-K"},
+    };
 
     public override void Initialize()
     {
@@ -33,34 +35,10 @@ public sealed class AnomalocaridAccentSystem : EntitySystem
     {
         var message = args.Message;
 
-        // blblbl
-        message = RegexLowerbubbleBL.Replace(message, "blblbl");
-        //
-        message = RegexUpperbubbleBL.Replace(message, "BLBLBL");
-        //
-        message = RegexCasebubbleBL.Replace(message, "Blblbl");
-        //
-        message = RegexCamelbubbleBL.Replace(message, "bLBLBL");
-        // glglgl
-        message = RegexLowerbubbleGL.Replace(message, "glglgl");
-        //
-        message = RegexUpperbubbleGL.Replace(message, "GLGLGL");
-        //
-        message = RegexCasebubbleGL.Replace(message, "Glglgl");
-        //
-        message = RegexCamelbubbleGL.Replace(message, "gLGLGL");
-        // k -> k-k
-        message = RegexLowerdoubleKK.Replace(message, "k-k");
-        //
-        message = RegexUpperdoubleKK.Replace(message, "K-K");
-        // ck -> ck-k
-        message = RegexLowerrepeatkCK.Replace(message, "ck-k");
-        //
-        message = RegexUpperrepeatkCK.Replace(message, "CK-K");
-        //
-        message = RegexCaserepeatkCK.Replace(message, "Ck-k");
-        //
-        message = RegexCamelrepeatkCK.Replace(message, "cK-K");
+        foreach (var keypair in Regexes)
+        {
+            message = keypair.Key.Replace(message, keypair.Value);
+        }
 
         message = _replacement.ApplyReplacements(message, "anomalocarid");
 
