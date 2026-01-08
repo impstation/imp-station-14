246e363ff4dfa136e729be9830dd973a71132e30
Darkmajia <darkwindleaf@hotmail.co.uk>
2026-01-08 13:22:00 +0000

    move emag checks to SharedThavenMoodsSystem

--- Content.Shared/_Impstation/Thaven/SharedThavenMoodsSystem.cs
+++ Content.Shared/_Impstation/Thaven/SharedThavenMoodsSystem.cs
@@ -1,11 +1,16 @@
 using Content.Shared._Impstation.StrangeMoods;
+using Content.Shared.Bed.Sleep;
 using Content.Shared.Emag.Systems;
+using Content.Shared.Mobs.Systems;
+using Content.Shared.Popups;
 
 namespace Content.Shared._Impstation.Thaven;
 
 public abstract class SharedThavenMoodsSystem : SharedStrangeMoodsSystem
 {
     [Dependency] private readonly EmagSystem _emag = default!;
+    [Dependency] private readonly MobStateSystem _mobState = default!;
+    [Dependency] private readonly SharedPopupSystem _popup = default!;
 
     public override void Initialize()
     {
@@ -21,6 +26,15 @@ public abstract class SharedThavenMoodsSystem : SharedStrangeMoodsSystem
 
         if (_emag.CheckFlag(ent, EmagType.Interaction))
             return;
+
+        // if the target is not sleeping, dead, or crit, skip.
+        var target = ent.Owner;
+        var user = args.UserUid;
+        if (!HasComp<SleepingComponent>(target) && !_mobState.IsIncapacitated(target) && target != user)
+        {
+            _popup.PopupClient(Loc.GetString("emag-thaven-alive", ("emag", ent), ("target", target)), user, user);
+            return;
+        }
 
         args.Handled = true;
     }
