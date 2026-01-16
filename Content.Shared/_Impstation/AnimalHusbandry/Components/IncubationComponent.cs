1d66a99fc4a933f75611bee478fa1f4b058e1ec1
FancyPlanks <123175013+FancyPlanks@users.noreply.github.com>
2026-01-16 05:51:06 +0000

    I'm not looking forward to the cleanup pass on where files are located i'll tell you that much

---
+++ Content.Shared/_Impstation/AnimalHusbandry/Components/IncubationComponent.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._Impstation.AnimalHusbandry.Components;
+
+/// <summary>
+/// Component used by the IncubationSystem to track what comes out, how long it takes to do so and other things
+/// </summary>
+[RegisterComponent]
+public sealed partial class IncubationComponent : Component
+{
+    [DataField("incubationTime")]
+    public TimeSpan IncubationTime = TimeSpan.FromSeconds(10);
+
+    // What comes out when the incubation is done?
+    [DataField("incubatedResult", required: true), ViewVariables(VVAccess.ReadWrite)]
+    public EntProtoId IncubatedResult;
+}
