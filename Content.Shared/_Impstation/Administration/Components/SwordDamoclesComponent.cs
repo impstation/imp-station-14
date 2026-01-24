3ca86662795bda7565276149e8a7d3825a739ba5
Simul-Ad-Astra <urface300@gmail.com>
2026-01-23 22:15:42 -0500

    oops

--- Content.Shared/_Impstation/Administration/Components/SwordDamoclesComponent.cs
+++ Content.Shared/_Impstation/Administration/Components/SwordDamoclesComponent.cs
@@ -7,21 +7,5 @@ namespace Content.Shared._Impstation.Administration.Components;
 [RegisterComponent, NetworkedComponent]
 public sealed partial class SwordDamoclesComponent : Component
 {
-    /// <summary>
-    ///     How many times has this smite been applied.
-    /// </summary>
-    [DataField]
-    public int TimesApplied = 0;
-
-    // <summary>
-    //      How much damage this smite should do.
-    // </summary>
-    [DataField]
-    public DamageSpecifier Damage = new()
-    {
-        DamageDict = new Dictionary<string, FixedPoint2>
-        {
-            {"Pierce", 30},
-        },
-    };
+    // yeah this doesn't have anything going on
 }
