cebcc98326aba1d5c4f821f419d7fc91e6a3e5fe
hivehum <ketchupfaced@gmail.com>
2026-02-04 20:56:31 -0500

    i cant test this because rider is imploding

--- Content.IntegrationTests/Tests/CargoTest.cs
+++ Content.IntegrationTests/Tests/CargoTest.cs
@@ -21,8 +21,8 @@ public sealed class CargoTest
 {
     private static readonly HashSet<ProtoId<CargoProductPrototype>> Ignored =
     [
-        new("FunCrateGambling"), // This is ignored because it is explicitly intended to be able to sell for more than it costs.
-        new("CrateValentines") // This is ignored because it is available as part of a limited time event.
+        // This is ignored because it is explicitly intended to be able to sell for more than it costs.
+        new("FunCrateGambling", "CrateValentines"), // imp edit - CrateValentines is ignored because it is available as a limited time event
     ];
 
     [Test]
