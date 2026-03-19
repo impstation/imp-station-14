e79b7e7b850361f1b619bfa57732ad8952dca3a0
salamanderskies <0technocraticaenigma0@gmail.com>
2026-03-19 10:50:40 -0400

    keb merge conflicts

--- Content.Shared/_Impstation/Heretic/Components/GhoulComponent.cs
+++ Content.Shared/_Impstation/Heretic/Components/GhoulComponent.cs
@@ -1,9 +1,14 @@
 using Content.Shared.FixedPoint;
 using Robust.Shared.GameStates;
+using Content.Shared.StatusIcon;
+using Robust.Shared.Prototypes;
 
 namespace Content.Shared._Impstation.Heretic;
 
 [RegisterComponent, NetworkedComponent]
+/// <summary>
+/// Component for Ghouls, dead bodies raised into servants by flesh heretics
+/// </summary>
 public sealed partial class GhoulComponent : Component
 {
     /// <summary>
@@ -20,4 +25,8 @@ public sealed partial class GhoulComponent : Component
     ///     Fallback for if dead threshold is null.
     /// </summary>
     [DataField] public FixedPoint2 FallbackHealth = 50;
+    /// <summary>
+    ///     Ghoul HUD icon.
+    /// </summary>
+    [DataField] public ProtoId<FactionIconPrototype> StatusIcon = "GhouledFaction";
 }
