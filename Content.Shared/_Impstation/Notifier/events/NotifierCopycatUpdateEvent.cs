3797c55caed9fa64a0fa0d5645a954cd9d454427
sev7ves <soapedzone@gmail.com>
2026-03-21 20:40:16 -0400

    oh yeah i didn't use this

--- Content.Shared/_Impstation/Notifier/events/NotifierCopycatUpdateEvent.cs
+++
@@ -1,6 +0,0 @@
-namespace Content.Shared._Impstation.Notifier.events;
-
-public sealed class NotifierCopycatUpdateEvent : EntityEventArgs
-{
-    public PlayerNotifierSettings Settings { get; set; } = new PlayerNotifierSettings();
-}
