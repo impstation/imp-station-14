dea844e1144259be1d04cc34ad9c7f59a2dea83a
sev7ves <58966116+sev7ves@users.noreply.github.com>
2026-03-23 15:31:15 -0400

    testing thiis setup

--- Content.Client/_Impstation/Notifier/NotifierSystem.cs
+++ Content.Client/_Impstation/Notifier/NotifierSystem.cs
@@ -2,6 +2,9 @@ using Content.Shared._Impstation.Notifier;
 
 namespace Content.Client._Impstation.Notifier;
 
+/// <summary>
+/// Empty system, required as the shared system is abstract.
+/// </summary>
 public sealed class NotifierSystem : SharedNotifierSystem
 {
 
