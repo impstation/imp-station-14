0be166b0cb11171be036fb7d57a20759e53ba904
jkwookee <157201244+jkwookee@users.noreply.github.com>
2026-03-28 16:06:15 -0400

    E

---
+++ Content.Server/_Impstation/StationEvents/Components/SupermatterSurgeRuleComponents.cs
@@ -0,0 +1,76 @@
+using Content.Server._Impstation.StationEvents.Events;
+
+namespace Content.Server._Impstation.StationEvents.Components;
+
+/// <summary>
+/// Used an event that increases the power of the Supermatter
+/// </summary>
+[RegisterComponent, Access(typeof(SupermatterSurgeRule))]
+public sealed partial class SupermatterSurgeRuleComponent : Component
+{
+    /// <summary>
+    /// The entity uid of the supermatter selected
+    /// </summary>
+    [DataField]
+    public EntityUid SupermatterUid;
+
+    /// <summary>
+    /// Minimum power that the supermatter can surge to
+    /// </summary>
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public float MinPowerSurge = 5000f;
+
+    /// <summary>
+    /// Maximum power that the supermatter can surge to
+    /// </summary>
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public float MaxPowerSurge = 11000f;
+
+    /// <summary>
+    /// Minimum heat modifier that the supermatter can surge to
+    /// </summary>
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public float MinHeatSurge = 1f;
+
+    /// <summary>
+    /// Maximum heat modifier that the supermatter can surge to
+    /// </summary>
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public float MaxHeatSurge = 2.5f;
+
+    /// <summary>
+    /// Time tracker for next lighting strike
+    /// </summary>
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public float TimeUntilNextLighting = 5f;
+
+    /// <summary>
+    /// Minimum time until next explosive lightning strike
+    /// </summary>
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public float MinTimeForLighting = 10f;
+
+    /// <summary>
+    /// Maximum time until next explosive lightning strike
+    /// </summary>
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public float MaxTimeForLighting = 20f;
+
+    /// <summary>
+    /// Range that the explosive lightning can strike in
+    /// </summary>
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public float ZapRange = 8f;
+
+    /// <summary>
+    /// Amount of explosive lightning strikes
+    /// </summary>
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public int ZapCount = 2;
+
+    /// <summary>
+    /// Recursive firing of lightning strikes
+    /// </summary>
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public int ZapArcDepth = 1;
+}
---
+++ Content.Server/_Impstation/StationEvents/Events/SupermatterSurgeRule.cs
@@ -0,0 +1,84 @@
+using Content.Server._Impstation.StationEvents.Components;
+using Content.Server.Announcements.Systems;
+using Content.Server.GameTicking;
+using Content.Server.Lightning;
+using Content.Server.StationEvents.Events;
+using Content.Shared._EE.Supermatter.Components;
+using Content.Shared.GameTicking.Components;
+using Content.Shared.Random.Helpers;
+using Robust.Shared.Player;
+using Robust.Shared.Random;
+
+namespace Content.Server._Impstation.StationEvents.Events;
+
+public sealed class SupermatterSurgeRule : StationEventSystem<SupermatterSurgeRuleComponent>
+{
+    [Dependency] private readonly AnnouncerSystem _announcer = default!;
+    [Dependency] private readonly LightningSystem _lightning = default!;
+    [Dependency] private readonly IRobustRandom _random = default!;
+    [Dependency] private readonly GameTicker _ticker = default!;
+
+    protected override void Added(EntityUid uid, SupermatterSurgeRuleComponent component, GameRuleComponent gameRule, GameRuleAddedEvent args)
+    {
+        base.Added(uid, component, gameRule, args);
+
+        var supermatterUids = new List<EntityUid>();
+        var query = EntityQueryEnumerator<SupermatterComponent>();
+
+        while (query.MoveNext(out var supermatterUid, out _))
+        {
+            supermatterUids.Add(supermatterUid);
+        }
+
+        if (supermatterUids.Count == 0)
+        {
+            // Means a scheduled event can be skipped on maps without supermatter, but hopefully the rarity of the event offsets that
+            _ticker.EndGameRule(uid, gameRule);
+            return;
+        }
+
+        component.SupermatterUid = _random.Pick(supermatterUids);
+
+        _announcer.SendAnnouncement(
+            _announcer.GetAnnouncementId(args.RuleId),
+            Filter.Broadcast(),
+            _announcer.GetEventLocaleString(_announcer.GetAnnouncementId(args.RuleId)),
+            colorOverride: Color.Gold
+        );
+    }
+
+    protected override void ActiveTick(EntityUid uid, SupermatterSurgeRuleComponent component, GameRuleComponent gameRule, float frameTime)
+    {
+        if (!TryComp<SupermatterComponent>(component.SupermatterUid, out var sm))
+            return;
+
+        sm.Surge = true;
+
+        var powerSurge = _random.NextFloat(component.MinPowerSurge, component.MaxPowerSurge);
+        var heatSurge = _random.NextFloat(component.MinHeatSurge, component.MaxHeatSurge);
+
+        // Power & heat modifer changes every tick so isn't always used by the supermatter, but creates a good visual on the console
+        sm.Power = powerSurge;
+        sm.HeatModifier = heatSurge;
+
+        if (component.TimeUntilNextLighting > 0)
+            component.TimeUntilNextLighting -= frameTime;
+        else
+        {
+            // Explosive supermatter lightning strikes
+            _lightning.ShootRandomLightnings(component.SupermatterUid, component.ZapRange, component.ZapCount, sm.LightningPrototypes[2], arcDepth: component.ZapArcDepth);
+
+            component.TimeUntilNextLighting += _random.NextFloat(component.MinTimeForLighting, component.MaxTimeForLighting);
+        }
+    }
+
+    protected override void Ended(EntityUid uid, SupermatterSurgeRuleComponent component, GameRuleComponent gameRule, GameRuleEndedEvent args)
+    {
+        base.Ended(uid, component, gameRule, args);
+
+        if (!TryComp<SupermatterComponent>(component.SupermatterUid, out var sm))
+            return;
+
+        sm.Surge = false;
+    }
+}
