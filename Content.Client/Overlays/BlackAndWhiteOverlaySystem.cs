09a86c80a4ed7a8460666dec909194bf8dfc85c0
sev7ves <soapedzone@gmail.com>
2026-03-30 17:27:35 -0400

    add accesibility handling

--- Content.Client/Overlays/BlackAndWhiteOverlaySystem.cs
+++ Content.Client/Overlays/BlackAndWhiteOverlaySystem.cs
@@ -1,12 +1,17 @@
+using Content.Shared._DV.CCVars;
 using Content.Shared.Inventory.Events;
 using Content.Shared.Overlays;
 using Robust.Client.Graphics;
+using Robust.Shared.Configuration;
+using Robust.Shared.Player;
 
 namespace Content.Client.Overlays;
 
 public sealed partial class BlackAndWhiteOverlaySystem : EquipmentHudSystem<BlackAndWhiteOverlayComponent>
 {
     [Dependency] private readonly IOverlayManager _overlayMan = default!;
+    [Dependency] private readonly IConfigurationManager _cfg = default!;
+    [Dependency] private readonly ISharedPlayerManager _playerMan = default!;
 
     private BlackAndWhiteOverlay _overlay = default!;
 
@@ -15,6 +20,36 @@ public sealed partial class BlackAndWhiteOverlaySystem : EquipmentHudSystem<Blac
         base.Initialize();
 
         _overlay = new();
+
+        SubscribeLocalEvent<BlackAndWhiteOverlayComponent, ComponentInit>(OnBWVisionInit);
+        SubscribeLocalEvent<BlackAndWhiteOverlayComponent, ComponentShutdown>(OnBWVisionShutdown);
+        SubscribeLocalEvent<BlackAndWhiteOverlayComponent, LocalPlayerAttachedEvent>(OnPlayerAttached);
+        SubscribeLocalEvent<BlackAndWhiteOverlayComponent, LocalPlayerDetachedEvent>(OnPlayerDetached);
+
+        Subs.CVar(_cfg, DCCVars.NoVisionFilters, OnNoVisionFiltersChanged);
+    }
+
+    private void OnPlayerDetached(Entity<BlackAndWhiteOverlayComponent> ent, ref LocalPlayerDetachedEvent args)
+    {
+        _overlayMan.RemoveOverlay(_overlay);
+    }
+
+    private void OnPlayerAttached(Entity<BlackAndWhiteOverlayComponent> ent, ref LocalPlayerAttachedEvent args)
+    {
+        if (!_cfg.GetCVar(DCCVars.NoVisionFilters))
+            _overlayMan.AddOverlay(_overlay);
+    }
+
+    private void OnBWVisionShutdown(Entity<BlackAndWhiteOverlayComponent> ent, ref ComponentShutdown args)
+    {
+        if (ent.Owner == _playerMan.LocalEntity)
+            _overlayMan.RemoveOverlay(_overlay);
+    }
+
+    private void OnBWVisionInit(Entity<BlackAndWhiteOverlayComponent> ent, ref ComponentInit args)
+    {
+        if (ent.Owner == _playerMan.LocalEntity && !_cfg.GetCVar(DCCVars.NoVisionFilters))
+            _overlayMan.AddOverlay(_overlay);
     }
 
     protected override void UpdateInternal(RefreshEquipmentHudEvent<BlackAndWhiteOverlayComponent> component)
@@ -29,5 +64,13 @@ public sealed partial class BlackAndWhiteOverlaySystem : EquipmentHudSystem<Blac
         base.DeactivateInternal();
 
         _overlayMan.RemoveOverlay(_overlay);
+    }
+
+    private void OnNoVisionFiltersChanged(bool enabled)
+    {
+        if (enabled)
+            _overlayMan.RemoveOverlay(_overlay);
+        else
+            _overlayMan.AddOverlay(_overlay);
     }
 }
