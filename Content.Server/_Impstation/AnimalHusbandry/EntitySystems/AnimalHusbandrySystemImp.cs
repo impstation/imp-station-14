dfabfdfeff406399e02cd0247be2e3fefd9fdec3
portfiend <109661617+portfiend@users.noreply.github.com>
2026-05-03 13:06:22 -0500

    move breeding functions into separate file i always hold the onion that if you're using C# regions you should probably be using `partial` instead

--- Content.Server/_Impstation/AnimalHusbandry/EntitySystems/AnimalHusbandrySystemImp.cs
+++ Content.Server/_Impstation/AnimalHusbandry/EntitySystems/AnimalHusbandrySystemImp.cs
@@ -1,15 +1,11 @@
-using System.Linq;
 using Content.Server.Administration.Logs;
-using Content.Shared.Damage.Components;
 using Content.Server.Cloning;
 using Content.Server.Ghost.Roles.Components;
 using Content.Shared.Database;
 using Content.Shared.EntityTable;
-using Content.Shared.Interaction.Components;
 using Content.Shared.Mind;
 using Content.Shared.Mobs.Components;
 using Content.Shared.Nutrition.AnimalHusbandry;
-using Content.Shared.Nutrition.Components;
 using Content.Shared.Nutrition.EntitySystems;
 using Robust.Shared.Audio.Systems;
 using Robust.Shared.Prototypes;
@@ -17,14 +13,13 @@ using Robust.Shared.Random;
 using Robust.Shared.Timing;
 
 using Content.Shared._Impstation.AnimalHusbandry.Components;
-using Content.Shared._Impstation.EntityTable.Conditions;
 
 namespace Content.Server._Impstation.AnimalHusbandry.EntitySystems;
 /// <summary>
 /// System that handles mob breeding, birthing and growing
 /// This system works alongside HTN
 /// </summary>
-public sealed class AnimalHusbandrySystemImp : EntitySystem
+public sealed partial class AnimalHusbandrySystemImp : EntitySystem
 {
     [Dependency] private readonly IAdminLogManager _adminLog = default!;
     [Dependency] private readonly IEntityManager _entManager = default!;
@@ -114,153 +109,6 @@ public sealed class AnimalHusbandrySystemImp : EntitySystem
         }
     }
 
-    #region BIRTHING
-
-    /// <summary>
-    /// Our function for handling the breeding action once all checks are finished and the
-    /// animal has approached its partner.
-    /// </summary>
-    /// <param name="approacher">The mob seeking to impregnate</param>
-    /// <param name="approached">The mob that will be impregnated</param>
-    /// <returns>True if the mob has successfully bred with the target</returns>
-    public bool TryBreedWithTarget(EntityUid approacher, EntityUid approached)
-    {
-        // Realistically this should never return false but it's just here for the moment
-        if (!_entManager.TryGetComponent<ImpReproductiveComponent>(approacher, out var component))
-            return false;
-
-        // Same with this one but i'm paranoid
-        if (!_entManager.TryGetComponent<ImpReproductiveComponent>(approached, out var partnerComp))
-            return false;
-
-        // Meta data of our approaching mob
-        if (!_entManager.TryGetComponent<MetaDataComponent>(approacher, out var meta))
-            return false;
-
-        if (meta.EntityPrototype == null)
-            return false;
-
-        // Just being 100% sure we aren't trying to self breed.
-        if (approacher == approached)
-            return false;
-
-        // one last check in case someone beat us to the cow or bred us on the way there
-        // It's dumb but unless I make the system assign pairings this is the best i can think of for the moment
-        if (!CanYouBreed((approacher, component)) || !CanYouBreed((approached, partnerComp)))
-            return false;
-
-        var partnerSettings = _prototype.Index(partnerComp.BreedSettings);
-
-        if (partnerSettings == null)
-            return false;
-
-        // Ready up for birth
-        partnerComp.Pregnant = true;
-        partnerComp.EndPregnancy = _time.CurTime.Add(partnerComp.PregnancyLength);
-
-        // Add them to our list of pregnant NPCs to be tracked
-        _mobsWaiting.Add(approached, partnerComp);
-
-        // Picks which offspring to give birth to based on the mob we bred with
-        var ctx = new EntityTableContext(new Dictionary<string, object>
-        {
-            { ValidPartnerCondition.PartnerContextKey, meta.EntityPrototype.ID },
-        });
-        partnerComp.MobToBirth = _entTable.GetSpawns(partnerSettings.PossibleInfants, ctx: ctx).ElementAt(0);
-
-        if (TryComp<InteractionPopupComponent>(approached, out var interactionPopup))
-            Spawn(interactionPopup.InteractSuccessSpawn, _transform.GetMapCoordinates(approached));
-
-        partnerComp.PreviousPartner = approacher;
-
-        // GET HUNGRY GET THIRSTY
-        _hunger.ModifyHunger(approacher, -component.HungerPerBirth);
-        _hunger.ModifyHunger(approached, -partnerComp.HungerPerBirth);
-
-        _thirst.ModifyThirst(approached, -component.HungerPerBirth);
-        _thirst.ModifyThirst(approached, -partnerComp.HungerPerBirth);
-
-        _adminLog.Add(LogType.Action, $"{ToPrettyString(approacher)} (carrier) and {ToPrettyString(approached)} (partner) successfully bred.");
-        return true;
-    }
-
-    /// <summary>
-    /// Checking if a given entity meets the criteria for breeding
-    /// This isn't set in stone to remain and it largely in this class for the moment so multiple scripts
-    /// Can use it, whether it remains is yet to be discovered. Depends on if it needs to be.
-    /// </summary>
-    /// <param name="entity">The fella we're checking out. Or ourself</param>
-    /// <returns>If the mob is eligible to breed</returns>
-    public bool CanYouBreed(Entity<ImpReproductiveComponent> entity)
-    {
-        if (entity.Comp.Pregnant)
-            return false;
-
-        // Making sure we're not trying to breed with an infant
-        if (_entManager.TryGetComponent<ImpInfantComponent>(entity, out var infant))
-            return false;
-
-        // Player inhabited mobs cannot breed
-        if (_mind.TryGetMind(entity, out var mind, out var mindComp))
-            return false;
-
-        if (_entManager.TryGetComponent<HungerComponent>(entity, out var hunger) && hunger.CurrentThreshold < HungerThreshold.Okay)
-            return false;
-
-        if (_entManager.TryGetComponent<ThirstComponent>(entity, out var thirst) && thirst.CurrentThirstThreshold < ThirstThreshold.Okay)
-            return false;
-
-        // A mob needs to be Alive. Not dead or critical
-        if (_entManager.TryGetComponent<MobStateComponent>(entity, out var state) && state.CurrentState != Shared.Mobs.MobState.Alive)
-            return false;
-
-        // A mob can't be too damaged
-        if (_entManager.TryGetComponent<DamageableComponent>(entity, out var damage) && damage.TotalDamage >= entity.Comp.MaxBreedDamage)
-            return false;
-        return true;
-    }
-
-    /// <summary>
-    /// Same as CanYouBreed except this one takes into account the animals search times
-    /// </summary>
-    /// <param name="entity">The mob checking if it's eligible to breed</param>
-    /// <returns>If the mob is eligible for breeding</returns>
-    public bool CanIBreed(Entity<ImpReproductiveComponent> entity)
-    {
-        if (entity.Comp.NextSearch > _time.CurTime)
-            return false;
-
-        if (!CanYouBreed(entity))
-            return false;
-
-        return true;
-    }
-
-    /// <summary>
-    /// Handles the actual birthing of the new NPC and sets how long until they grow up
-    /// </summary>
-    /// <param name="entity">The mob giving birth</param>
-    private void Birth(Entity<ImpReproductiveComponent> entity)
-    {
-        if (TryComp<InteractionPopupComponent>(entity, out var interactionPopup))
-            _audio.PlayPvs(interactionPopup.InteractSuccessSound, entity);
-
-        var offspring = SpawnNewMob(entity, entity.Comp.MobToBirth);
-
-        if (offspring == null)
-            return;
-
-        if (_entManager.TryGetComponent<ImpInfantComponent>(offspring, out var infantComp))
-        {
-            infantComp.GrowthTimeRemaining = _time.CurTime.Add(infantComp.GrowthTime);
-            infantComp.Parent = entity;
-        }
-    }
-
-    #endregion
-
-    #region INFANTS
-
     /// <summary>
     /// Handles growing an infant by deleting the current mob and making a new one
     /// This also transfers the previous components to the new mob
@@ -306,10 +154,6 @@ public sealed class AnimalHusbandrySystemImp : EntitySystem
         return isAdult;
     }
 
-    #endregion
-
-    #region UNIVERSAL
-
     /// <summary>
     /// Handles spawning a new mob for us
     /// </summary>
@@ -324,6 +168,4 @@ public sealed class AnimalHusbandrySystemImp : EntitySystem
 
         return newMob;
     }
-
-    #endregion
 }
