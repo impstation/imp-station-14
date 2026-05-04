118936ccb9b38e0aa46e9fefae13a1850b1eeb3e
portfiend <109661617+portfiend@users.noreply.github.com>
2026-05-04 16:55:03 -0500

    oops

--- Content.Server/_Impstation/NPC/HTN/Preconditions/BreedablePrecondition.cs
+++ Content.Server/_Impstation/NPC/HTN/Preconditions/BreedablePrecondition.cs
@@ -14,7 +14,6 @@ namespace Content.Server._Impstation.NPC.HTN.Preconditions;
 /// </summary>
 public sealed partial class BreedablePrecondition : HTNPrecondition
 {
-    [Dependency] private readonly AnimalHusbandrySystemImp _husbandry = default!;
     [Dependency] private readonly IEntityManager _entityManager = default!;
     [Dependency] private readonly IGameTiming _time = default!;
     [Dependency] private readonly IRobustRandom _random = default!;
@@ -61,7 +60,7 @@ public sealed partial class BreedablePrecondition : HTNPrecondition
 
         // Sets the amount of time until they try and find a new mate.
         // It's partly so all mobs aren't just in sync, but also so the server isn't spamming searches.
-        _husbandry.RefreshSearchTime(reproducer);
+        _breedSystem.RefreshSearchTime(reproducer);
 
         return true;
     }
