e56617bd104dc67fff08f536b2de5e738f4d1c9e
msTheowo <31812973+msTheowo@users.noreply.github.com>
2026-05-20 10:43:29 -0400

    Junior Lone Operatives

---
+++ Content.Server/_Impstation/Ghost/Components/GhostRoleRandomHumanoidSpawnerComponent.cs
@@ -0,0 +1,18 @@
+﻿using Content.Shared.Humanoid.Prototypes;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
+
+namespace Content.Server._Impstation.Ghost.Components;
+
+/// <summary>
+/// A messed up fusion of GhostRoleMobSpawnerComponent and RandomHumanoidSpawnerComponent, letting ghost roles spawn as random humanoids.
+/// </summary>
+[RegisterComponent, EntityCategory("Spawner")]
+public sealed partial class GhostRoleRandomHumanoidSpawnerComponent : Component
+{
+    /// <summary>
+    /// The RandomHumanoidSettingsPrototype to use for this spawner.
+    /// </summary>
+    [DataField("settings", customTypeSerializer: typeof(PrototypeIdSerializer<RandomHumanoidSettingsPrototype>), required: true)]
+    public string? SettingsPrototypeId;
+}
