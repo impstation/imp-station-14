34446563dfff976e49ec5f4e05a20570a400524b
jkwookee <157201244+jkwookee@users.noreply.github.com>
2026-05-21 16:17:28 -0400

    move to upstream rule

--- Content.Server/StationEvents/Components/RandomSpawnRuleComponent.cs
+++ Content.Server/StationEvents/Components/RandomSpawnRuleComponent.cs
@@ -1,11 +1,13 @@
 ﻿using Content.Server.StationEvents.Events;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
+using Content.Shared.Destructible.Thresholds; // Imp
 
 namespace Content.Server.StationEvents.Components;
 
 /// <summary>
-/// Spawns a single entity at a random tile on a station using TryGetRandomTile.
+/// Imp, changed description.
+/// Spawns specified amount of entity at a random or empty tile on a station using TryGetRandomTile.
 /// </summary>
 [RegisterComponent, Access(typeof(RandomSpawnRule))]
 public sealed partial class RandomSpawnRuleComponent : Component
@@ -15,4 +17,16 @@ public sealed partial class RandomSpawnRuleComponent : Component
     /// </summary>
     [DataField("prototype", required: true, customTypeSerializer: typeof(PrototypeIdSerializer<EntityPrototype>))]
     public string Prototype = string.Empty;
+
+    [DataField]
+    public EntProtoId? SpawnEffect; // Imp
+
+    [DataField]
+    public MinMax MinMaxEntities = new(1, 1); // Imp
+
+    [DataField]
+    public bool EmptyTilesOnly; // Imp
+
+    [DataField]
+    public LocId? Announcement; // Imp
 }
--- Content.Server/StationEvents/Events/RandomSpawnRule.cs
+++ Content.Server/StationEvents/Events/RandomSpawnRule.cs
@@ -1,19 +1,66 @@
 using Content.Server.GameTicking.Rules.Components;
 using Content.Server.StationEvents.Components;
 using Content.Shared.GameTicking.Components;
+// Imp start
+using Content.Server.Announcements.Systems;
+using Content.Shared.Coordinates;
+using Robust.Shared.Player;
+using Robust.Shared.Random;
+// Imp end
 
 namespace Content.Server.StationEvents.Events;
 
 public sealed class RandomSpawnRule : StationEventSystem<RandomSpawnRuleComponent>
 {
+    // Imp start
+    [Dependency] private readonly IRobustRandom _random = default!;
+    [Dependency] private readonly AnnouncerSystem _announcer = default!;
+    [Dependency] private readonly EntityLookupSystem _lookup = default!;
+
+    // Remove this when announcement system allows for specified announcements instead of deriving them from the event name
+    // Or when the EE announcer system is refactored to support delays
+    protected override void Added(EntityUid uid, RandomSpawnRuleComponent component, GameRuleComponent gameRule, GameRuleAddedEvent args)
+    {
+        base.Added(uid, component, gameRule, args);
+
+        if (component.Announcement == null)
+            return;
+
+        _announcer.SendAnnouncement(
+            _announcer.GetAnnouncementId(args.RuleId),
+            Filter.Broadcast(),
+            component.Announcement,
+            colorOverride: Color.Gold);
+    }
+    // Imp end
+
     protected override void Started(EntityUid uid, RandomSpawnRuleComponent comp, GameRuleComponent gameRule, GameRuleStartedEvent args)
     {
         base.Started(uid, comp, gameRule, args);
 
-        if (TryFindRandomTile(out _, out _, out _, out var coords))
+        // Imp start, added MinMax & ability to check tiles for entities before spawning
+        var attempt = 0;
+        var total = comp.MinMaxEntities.Next(_random);
+        for (var i = 0; i < total; i++)
         {
+            if (!TryFindRandomTile(out var tileIndices, out _, out var grid, out var coords))
+                continue;
+
+            if (comp.EmptyTilesOnly)
+            {
+                // Ignore tiles with mobs or machines
+                var entities = _lookup.GetLocalEntitiesIntersecting(grid, tileIndices, flags: LookupFlags.Dynamic | LookupFlags.Static);
+                if (entities.Count != 0 && attempt < 100) // If it fails that much just let it spawn
+                {
+                    attempt++;
+                    i--;
+                    continue;
+                }
+            }
+            // Imp end
             Sawmill.Info($"Spawning {comp.Prototype} at {coords}");
             Spawn(comp.Prototype, coords);
+            Spawn(comp.SpawnEffect, coords); // Imp, added effects
         }
     }
 }
