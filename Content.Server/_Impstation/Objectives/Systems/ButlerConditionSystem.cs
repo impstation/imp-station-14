637317de838305b2daa66edcb4585f61c0e9b3c8
jkwookee <157201244+jkwookee@users.noreply.github.com>
2026-05-22 07:24:17 -0400

    fixes for alot of stuff

--- Content.Server/_Impstation/Objectives/Systems/ButlerConditionSystem.cs
+++ Content.Server/_Impstation/Objectives/Systems/ButlerConditionSystem.cs
@@ -1,11 +1,16 @@
 using Content.Server._Impstation.Objectives.Components;
+using Content.Server.DeviceLinking.Components;
 using Content.Server.Objectives.Systems;
 using Content.Shared.Hands.EntitySystems;
+using Content.Shared.Implants.Components;
 using Content.Shared.Inventory;
 using Content.Shared.Mind;
 using Content.Shared.Objectives.Components;
 using Content.Shared.Popups;
+using Content.Shared.Storage;
 using Content.Shared.Storage.EntitySystems;
+using Robust.Server.Audio;
+using Robust.Shared.Player;
 
 namespace Content.Server._Impstation.Objectives.Systems;
 
@@ -14,7 +19,9 @@ namespace Content.Server._Impstation.Objectives.Systems;
 /// </summary>
 public sealed class ButlerConditionSystem : EntitySystem
 {
+    [Dependency] private readonly AudioSystem _audio = default!;
     [Dependency] private readonly InventorySystem _inventory = default!;
+    [Dependency] private readonly ISharedPlayerManager _player = default!;
     [Dependency] private readonly SharedHandsSystem _hands = default!;
     [Dependency] private readonly SharedPopupSystem _popup = default!;
     [Dependency] private readonly SharedStorageSystem _storage = default!;
@@ -34,24 +41,44 @@ public sealed class ButlerConditionSystem : EntitySystem
     /// </summary>
     private void OnAfterAssign(Entity<ButlerConditionComponent> ent, ref ObjectiveAfterAssignEvent args)
     {
-        if (!_target.GetTarget(ent, out var target)) //get the butler target
+        if (!_target.GetTarget(ent, out var target)) // get the butler target
             return;
 
         if (!TryComp<MindComponent>(target, out var mind) || mind.CurrentEntity is not { } mindBody)
             return;
 
-        var coords = _transform.GetMapCoordinates(mindBody);
+        if (!_player.TryGetSessionById(mind.UserId, out var session))
+            return;
+
         _popup.PopupEntity(Loc.GetString(ent.Comp.ButlerSpawn), mindBody, mindBody);
+        _audio.PlayGlobal(ent.Comp.TargetAudio, session);
+
         // give the target the remote
-        var remote = Spawn(ent.Comp.Package, coords);
+        var coords = _transform.GetMapCoordinates(mindBody);
+        var package = Spawn(ent.Comp.Package, coords);
+
+        if (!TryComp<StorageComponent>(package, out var storage))
+            return;
+
+        foreach (var (item, _) in storage.StoredItems)
+            RemCompDeferred<AutoLinkTransmitterComponent>(item);
+
+        if (args.Mind.CurrentEntity is { })
+            RemCompDeferred<AutoLinkReceiverComponent>(args.Mind.CurrentEntity.Value);
+
+        if (!TryComp<ImplantedComponent>(args.Mind.CurrentEntity, out var implanted))
+            return;
+
+        foreach (var implant in implanted.ImplantContainer.ContainedEntities)
+            RemCompDeferred<AutoLinkReceiverComponent>(implant);
 
         if (!_inventory.TryGetSlotEntity(mindBody, Slot, out var backpack) ||
-            !_storage.Insert(backpack.Value, remote, out _)) //bag is full, put in hand
+            !_storage.Insert(backpack.Value, package, out _)) //bag is full, put in hand
         {
-            _hands.TryPickup(mindBody, remote);
+            _hands.TryPickup(mindBody, package);
             return;
         }
         // no bag somehow, at least pick it up
-        _hands.TryPickup(mindBody, remote);
+        _hands.TryPickup(mindBody, package);
     }
 }
