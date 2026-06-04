9db33bc20d7278788910a34df1e5ba03533c8382
sev7ves <58966116+sev7ves@users.noreply.github.com>
2026-06-04 19:08:33 -0400

    the main mechanic is working

--- Content.Shared/_Impstation/Tools/Components/KeyRingComponent.cs
+++ Content.Shared/_Impstation/Tools/Components/KeyRingComponent.cs
@@ -1,17 +1,23 @@
 
 using Content.Shared.Access;
+using Robust.Shared.GameStates;
+using Robust.Shared.Serialization;
 
 namespace Content.Shared._Impstation.Tools.Components;
 
-[RegisterComponent]
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState(true)]
 public sealed partial class KeyRingComponent : Component
 {
-    /// <summary>
-    /// Queue representing the key cards on the ring
-    /// </summary>
-    public Queue<AccessLevelPrototype> KeyCards = new();
+    [DataField, AutoNetworkedField]
+    public TimeSpan UseDelay = TimeSpan.Zero;
 
     [DataField("blacklist")]
     public HashSet<AccessLevelPrototype> Blacklist = new();
+
+    [DataField("minUseTime")]
+    public double MinUseTime = 15;
+
+    [DataField("maxUseTime")]
+    public double MaxUseTime = 30;
 }
 
--- Content.Shared/_Impstation/Tools/Systems/KeyRingSystem.cs
+++ Content.Shared/_Impstation/Tools/Systems/KeyRingSystem.cs
@@ -5,12 +5,16 @@ using Content.Shared._Impstation.Tools.Components;
 using Content.Shared.Access;
 using Content.Shared.Access.Components;
 using Content.Shared.Access.Systems;
+using Content.Shared.Administration.Logs;
+using Content.Shared.Database;
 using Content.Shared.DoAfter;
 using Content.Shared.Doors.Components;
 using Content.Shared.Doors.Systems;
+using Content.Shared.Emag.Systems;
 using Content.Shared.Interaction;
 using Content.Shared.Lock;
 using Robust.Shared.Prototypes;
+using Robust.Shared.Random;
 using Robust.Shared.Serialization;
 using Robust.Shared.Timing;
 
@@ -20,54 +24,74 @@ public sealed partial class KeyRingSystem : EntitySystem
 {
     [Dependency] private IPrototypeManager _prototypeManager = default!;
     [Dependency] private SharedDoorSystem _doorSystem = default!;
+    [Dependency] private AccessReaderSystem _accessReaderSystem = default!;
+    [Dependency] private LockSystem _lockSystem = default!;
     [Dependency] private SharedDoAfterSystem _doAfter = default!;
+    [Dependency] private ISharedAdminLogManager _adminLogger = default!;
+    [Dependency] private IGameTiming _timing = default!;
 
-    [Dependency] protected IGameTiming Timing = default!;
+    [Dependency] private IRobustRandom _random = default!;
 
     [Serializable, NetSerializable]
-    public sealed partial class KeyRingDoAfterEvent : SimpleDoAfterEvent;
+    public sealed partial class KeyRingDoorDoAfterEvent : SimpleDoAfterEvent;
+
+    [Serializable, NetSerializable]
+    public sealed partial class KeyRingLockDoAfterEvent : SimpleDoAfterEvent;
     public override void Initialize()
     {
         base.Initialize();
 
-        SubscribeLocalEvent<KeyRingComponent, ComponentInit>(OnComponentInit);
-        SubscribeLocalEvent<KeyRingComponent, UserInteractUsingEvent>(TryStartKeyCardDoAfter);
-        SubscribeLocalEvent<KeyRingComponent, KeyRingDoAfterEvent>(KeyCardDoAfter);
+        SubscribeLocalEvent<KeyRingComponent, ComponentStartup>(OnComponentStartup);
+        SubscribeLocalEvent<KeyRingComponent, AfterInteractEvent>(TryStartKeyCardDoAfter);
+        SubscribeLocalEvent<KeyRingComponent, KeyRingDoorDoAfterEvent>(KeyCardDoorDoAfter);
+        SubscribeLocalEvent<KeyRingComponent, KeyRingLockDoAfterEvent>(KeyCardLockDoAfter);
     }
 
-    private void OnComponentInit(Entity<KeyRingComponent> ent, ref ComponentInit args)
+    private void OnComponentStartup(Entity<KeyRingComponent> ent, ref ComponentStartup args)
     {
-        var accessLevels = _prototypeManager.EnumeratePrototypes<AccessLevelPrototype>();
-
-        foreach (var access in accessLevels)
-        {
-            if (ent.Comp.Blacklist.Contains(access))
-                continue;
-            ent.Comp.KeyCards.Append(access);
-        }
-        ent.Comp.KeyCards.Shuffle();
+        ent.Comp.UseDelay = TimeSpan.FromSeconds(_random.NextDouble(ent.Comp.MinUseTime,ent.Comp.MaxUseTime));
+        Dirty(ent);
     }
 
-    private void TryStartKeyCardDoAfter(Entity<KeyRingComponent> ent, ref UserInteractUsingEvent args)
+    private void TryStartKeyCardDoAfter(Entity<KeyRingComponent> ent, ref AfterInteractEvent args)
     {
         if (!TryComp<AccessReaderComponent>(args.Target, out var accessReader))
             return;
 
-        var doargs = new DoAfterArgs(EntityManager, ent, TimeSpan.FromSeconds(5d), new KeyRingDoAfterEvent(), ent, args.Target, args.Used)
+        DoAfterArgs? doargs = null;
+
+        if (HasComp<DoorComponent>(args.Target)){
+            doargs = new DoAfterArgs(EntityManager, args.User, ent.Comp.UseDelay, new KeyRingDoorDoAfterEvent(), ent, target: args.Target, args.Used)
+            {
+                BreakOnDamage = true,
+                BreakOnHandChange = true,
+                BreakOnMove = true,
+                BreakOnWeightlessMove = true,
+            };
+        }
+        else if (HasComp<LockComponent>(args.Target))
         {
-            BreakOnDamage = true,
-            BreakOnHandChange = true,
-            BreakOnMove = true,
-            BreakOnWeightlessMove = true,
-            AttemptFrequency = AttemptFrequency.StartAndEnd
-        };
+            doargs = new DoAfterArgs(EntityManager, args.User, ent.Comp.UseDelay, new KeyRingLockDoAfterEvent(), ent, target: args.Target, args.Used)
+            {
+                BreakOnDamage = true,
+                BreakOnHandChange = true,
+                BreakOnMove = true,
+                BreakOnWeightlessMove = true,
+            };
+        }
+        else
+        {
+            args.Handled = true;
+            return;
+        }
+
         _doAfter.TryStartDoAfter(doargs);
         args.Handled = true;
     }
 
-    private void KeyCardDoAfter(Entity<KeyRingComponent> ent, ref KeyRingDoAfterEvent args)
+    private void KeyCardDoorDoAfter(Entity<KeyRingComponent> ent, ref KeyRingDoorDoAfterEvent args)
     {
-        if (!Timing.IsFirstTimePredicted)
+        if (!_timing.IsFirstTimePredicted)
             return;
 
         if (args.Target == null||args.Cancelled)//if the target somehow dissapears or the action was cancelled then return
@@ -75,5 +99,58 @@ public sealed partial class KeyRingSystem : EntitySystem
             return;
         }
 
+        if (!TryComp<DoorComponent>(args.Target.Value, out var doorComp)||!_accessReaderSystem.GetMainAccessReader(args.Target.Value, out var accessReader))
+            return;
+
+        var accessComponent = accessReader.Value.Comp;
+        var isAirlock = TryComp<AirlockComponent>(args.Target, out var airlockComp);
+
+        foreach (var accessList in accessComponent.AccessLists)
+        {
+            foreach (var accessType in ent.Comp.Blacklist)
+            {
+                if (!accessList.Contains(accessType))
+                    continue;
+                if (isAirlock)
+                    _doorSystem.Deny(args.Target.Value, doorComp, user: args.User, predicted: true);
+                return;
+            }
+        }
+
+        if (_doorSystem.TryToggleDoor(args.Target.Value, doorComp, user: args.User, predicted: true)){
+            _adminLogger.Add(LogType.Action,
+                            LogImpact.Medium,
+                            $"{ToPrettyString(args.User):player} used {ToPrettyString(args.Used)} on {ToPrettyString(args.Target.Value)}: {doorComp.State}");
+        }
+        ent.Comp.UseDelay = TimeSpan.FromSeconds(_random.NextDouble(ent.Comp.MinUseTime,ent.Comp.MaxUseTime));
+        Dirty(ent);
+    }
+
+    private void KeyCardLockDoAfter(Entity<KeyRingComponent> ent, ref KeyRingLockDoAfterEvent args)
+    {
+        if (!_timing.IsFirstTimePredicted)
+            return;
+
+        if (args.Target == null||args.Cancelled)//if the target somehow dissapears or the action was cancelled then return
+        {
+            return;
+        }
+        if (!TryComp<LockComponent>(args.Target.Value, out var lockComponent)||!_accessReaderSystem.GetMainAccessReader(args.Target.Value, out var accessReader))
+            return;
+
+        var accessComponent = accessReader.Value.Comp;
+        foreach (var accessList in accessComponent.AccessLists)
+        {
+            foreach (var accessType in ent.Comp.Blacklist)
+            {
+                if (!accessList.Contains(accessType))
+                    continue;
+                return;
+            }
+        }
+        _lockSystem.ToggleLock(args.Target.Value, args.User, lockComponent);
+        _adminLogger.Add(LogType.Action,
+                            LogImpact.Medium,
+                            $"{ToPrettyString(args.User):player} used {ToPrettyString(args.Used)} on {ToPrettyString(args.Target.Value)} locked: {lockComponent.Locked}");
     }
 }
