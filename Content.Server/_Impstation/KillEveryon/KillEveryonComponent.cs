c9d3b9e8fd6f5de7200d05f3fbba3c0877dfd306
sev7ves <58966116+sev7ves@users.noreply.github.com>
2026-06-13 19:18:33 -0400

    kill everyon

---
+++ Content.Server/_Impstation/KillEveryon/KillEveryonComponent.cs
@@ -0,0 +1,8 @@
+namespace Content.Server._Impstation.KillEveryon;
+
+[RegisterComponent]
+public sealed partial class KillEveryonComponent : Component
+{
+    [DataField]
+    public string Role = "MindRoleKillEveryon";
+}
---
+++ Content.Server/_Impstation/KillEveryon/KillEveryonSystem.cs
@@ -0,0 +1,29 @@
+using Content.Server.Antag;
+using Content.Server.Ghost.Roles.Components;
+using Content.Server.Mind;
+using Content.Shared.Mind;
+using Content.Shared.Players;
+using Content.Shared.Roles;
+using Content.Shared.Roles.Components;
+
+namespace Content.Server._Impstation.KillEveryon;
+
+
+public sealed partial class KillEveryonSystem : EntitySystem
+{
+    [Dependency] private readonly SharedRoleSystem _roles = default!;
+    [Dependency] private readonly MindSystem _mind = default!;
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<KillEveryonComponent, TakeGhostRoleEvent>(KillEveryon);
+    }
+
+    private void KillEveryon(Entity<KillEveryonComponent> ent, ref TakeGhostRoleEvent args)
+    {
+        if (!_mind.TryGetMind(args.Player, out var mindId, out var mind))
+            return;
+        _mind.TryAddObjective(mindId, mind, "KillEveryonObjective");
+    }
+}
