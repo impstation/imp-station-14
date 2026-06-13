ef0e51cb759dcbeda27a0f9975a8ae9c8179c5f4
starlighthowls <starlighthowls7@gmail.com>
2026-06-13 14:39:51 +0100

    wip

---
+++ Content.Server/_Impstation/Roles/HoundRoleComponent.cs
@@ -0,0 +1,11 @@
+using Content.Shared.Roles.Components;
+
+namespace Content.Server._Impstation.Roles;
+
+/// <summary>
+///     Added to mind role entities to tag that they are the hound of whimsy
+/// </summary>
+[RegisterComponent]
+public sealed partial class HoundRoleComponent : BaseMindRoleComponent
+{
+}
