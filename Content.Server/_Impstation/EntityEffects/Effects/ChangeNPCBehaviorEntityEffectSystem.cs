ea9a386c5c722d204d0d9b70a9704d7429338265
coyragon96 <191927940+coyragon96@users.noreply.github.com>
2026-07-13 11:11:46 -0500

    Zoochem update

--- Content.Server/_Impstation/EntityEffects/Effects/ChangeNPCBehaviorEntityEffectSystem.cs
+++ Content.Server/_Impstation/EntityEffects/Effects/ChangeNPCBehaviorEntityEffectSystem.cs
@@ -1,5 +1,7 @@
 using Content.Server.NPC.HTN;
 using Content.Server.NPC.Systems;
+using Content.Server.Ghost.Roles.Components;
+using Content.Server.StationEvents.Components;
 using Content.Shared.EntityEffects;
 using Content.Shared._Impstation.EntityEffects.Effects;
 using Content.Shared.NPC;
@@ -7,7 +9,7 @@ using Content.Shared.NPC;
 namespace Content.Server._Impstation.EntityEffects.Effects;
 
 /// <summary>
-/// If target has HTNComponent, changes their root task and resets their targeting.
+/// If target has HTNComponent, changes their root task, resets their targeting, removes their ghost role, and removes their ability to be targeted by the random sentience event.
 /// </summary>
 /// <inheritdoc cref="EntityEffectSystem{T,TEffect}"/>
 public sealed partial class ChangeNPCBehaviorEntityEffectSystem : EntityEffectSystem<HTNComponent, ChangeNPCBehavior>
@@ -21,5 +23,9 @@ public sealed partial class ChangeNPCBehaviorEntityEffectSystem : EntityEffectSy
 
         // Resets their targeting.
         htn.Blackboard.Remove<EntityUid>("Target");
+
+        // Removes their ghost role and ability to be randomly made sentient.
+        RemComp<GhostRoleComponent>(entity);
+        RemComp<SentienceTargetComponent>(entity);
     }
 }
