40499d515809ceec6a398ace808a8bc6bb4bbe6e
coyragon96 <191927940+coyragon96@users.noreply.github.com>
2026-07-14 22:20:01 -0500

    I should not have trusted upstream code

--- Content.Server/_Impstation/EntityEffects/Effects/ChangeNPCBehaviorEntityEffectSystem.cs
+++ Content.Server/_Impstation/EntityEffects/Effects/ChangeNPCBehaviorEntityEffectSystem.cs
@@ -10,7 +10,6 @@ namespace Content.Server._Impstation.EntityEffects.Effects;
 /// <inheritdoc cref="EntityEffectSystem{T,TEffect}"/>
 public sealed partial class ChangeNPCBehaviorEntityEffectSystem : EntityEffectSystem<HTNComponent, ChangeNPCBehavior>
 {
-    [Dependency] private readonly HTNSystem _htn = default!;
     protected override void Effect(Entity<HTNComponent> entity, ref EntityEffectEvent<ChangeNPCBehavior> args)
     {
         // Changes the targets root task to whatever htnCompound is set.
