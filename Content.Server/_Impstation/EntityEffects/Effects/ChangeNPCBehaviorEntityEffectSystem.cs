071cf875a98231b98bf164a26f38f8c1e56c16b5
coyragon96 <191927940+coyragon96@users.noreply.github.com>
2026-07-21 13:22:36 -0500

    yeah

--- Content.Server/_Impstation/EntityEffects/Effects/ChangeNPCBehaviorEntityEffectSystem.cs
+++ Content.Server/_Impstation/EntityEffects/Effects/ChangeNPCBehaviorEntityEffectSystem.cs
@@ -1,11 +1,12 @@
 using Content.Server.NPC.HTN;
+using Content.Shared.NPC.Components;
 using Content.Shared.EntityEffects;
 using Content.Shared._Impstation.EntityEffects.Effects;
 
 namespace Content.Server._Impstation.EntityEffects.Effects;
 
 /// <summary>
-/// If target has HTNComponent, changes their root task and resets their targeting.
+/// If target has HTNComponent, changes their root task, removes FactionExceptionComponent, and resets their targeting.
 /// </summary>
 /// <inheritdoc cref="EntityEffectSystem{T,TEffect}"/>
 public sealed partial class ChangeNPCBehaviorEntityEffectSystem : EntityEffectSystem<HTNComponent, ChangeNPCBehavior>
@@ -15,6 +16,9 @@ public sealed partial class ChangeNPCBehaviorEntityEffectSystem : EntityEffectSy
         // Changes the targets root task to whatever htnCompound is set.
         var htn = entity.Comp;
         htn.RootTask = new HTNCompoundTask { Task = args.Effect.rootTask };
+
+        // Removes FactionExceptionComponent
+        RemComp<FactionExceptionComponent>(entity);
 
         // Resets their targeting.
         htn.Blackboard.Remove<EntityUid>("Target");
