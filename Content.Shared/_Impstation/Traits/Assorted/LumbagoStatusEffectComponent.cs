4e98a37e7ec03ab06dc0b3a91dea0b2b91001ca8
sev7ves <58966116+sev7ves@users.noreply.github.com>
2026-08-12 16:41:09 -0400

    Yuppppp

---
+++ Content.Shared/_Impstation/Traits/Assorted/LumbagoStatusEffectComponent.cs
@@ -0,0 +1,70 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._Impstation.Traits.Assorted;
+
+/// <summary>
+/// A status effect meant to replicate lumbago, aka lower back pain.
+/// Occasionally send popups about back pain, makes pulling slower, and occasionally causes a blanket move speed debuff.
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class LumbagoStatusEffectComponent : Component
+{
+    /// <summary>
+    /// The probability of a reminder popup appearing every second.
+    /// </summary>
+    [DataField]
+    public float LumbagoReminderChance = 0.05f;
+
+    /// <summary>
+    /// The probability of a flare up starting every second.
+    /// </summary>
+    [DataField]
+    public float FlareUpChance = 0.005f;
+
+    /// <summary>
+    /// The effects pulling walk speed modifier
+    /// </summary>
+    [DataField]
+    public float PullWalkSpeedMod = 0.5f;
+
+    /// <summary>
+    /// The effects sprinting walk speed modifier
+    /// </summary>
+    [DataField]
+    public float PullSprintSpeedMod = 0.5f;
+
+    /// <summary>
+    /// The minimum duration of a flare up.
+    /// </summary>
+    [DataField]
+    public float FlareUpDurationMin = 10;
+
+    /// <summary>
+    /// The maximum duration of a flare up.
+    /// </summary>
+    [DataField]
+    public float FlareUpDurationMax = 60;
+
+    /// <summary>
+    /// The blanket move speed modifier of a flare up.
+    /// </summary>
+    [DataField]
+    public float FlareUpMovementSpeedMod = 0.5f;
+
+    /// <summary>
+    /// The set of reminders.
+    /// </summary>
+    [DataField]
+    public List<string> MildPainReminders = new();
+
+    /// <summary>
+    /// The set of reminders during a flair up.
+    /// </summary>
+    [DataField]
+    public List<string> BadPainReminders = new();
+
+    /// <summary>
+    /// The target entity of the status effect.
+    /// </summary>
+    public EntityUid Affected;
+}
