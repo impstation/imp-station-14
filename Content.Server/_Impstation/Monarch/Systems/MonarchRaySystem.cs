5fb37427042230c2b445aa47be6020e093952752
PaigeM~ <roseliasquartz@gmail.com>
2026-08-22 15:31:01 -0700

    on host death

--- Content.Server/_Impstation/Monarch/Systems/MonarchRaySystem.cs
+++ Content.Server/_Impstation/Monarch/Systems/MonarchRaySystem.cs
@@ -1,12 +1,15 @@
 using Content.Server._Impstation.Monarch.Events;
 using Content.Server._Impstation.Monarch.Components;
 using Content.Shared._Impstation.Consume.Components;
+using Content.Server.Ghost;
 using Content.Shared.Clothing;
 using Content.Shared.Clothing.Components;
 using Content.Shared.Damage;
+using Content.Shared.Damage.Systems;
+using Content.Shared.Inventory;
 using Content.Shared.Players;
 using Content.Shared.Mind;
-using Content.Shared.Damage.Systems;
+using Content.Shared.Mobs;
 using Robust.Shared.Audio;
 using Robust.Shared.Audio.Systems;
 using Robust.Shared.Player;
@@ -17,6 +20,7 @@ public sealed class MonarchRaySystem : EntitySystem
 {
     [Dependency] private readonly IPrototypeManager _protoMan = default!;
     [Dependency] private readonly SharedAudioSystem _audioSystem = default!;
+    [Dependency] private readonly InventorySystem _inventorySystem = default!;
     [Dependency] private readonly SharedMindSystem _mindSystem = default!;
     [Dependency] private readonly DamageableSystem _damageSystem = default!;
 
@@ -24,24 +28,41 @@ public sealed class MonarchRaySystem : EntitySystem
     {
         SubscribeLocalEvent<MonarchRayComponent, ClothingGotEquippedEvent>(OnClothingEquip);
         SubscribeLocalEvent<MonarchRayComponent, ClothingGotUnequippedEvent>(OnClothingUnequip);
+
+        SubscribeLocalEvent<MonarchRayHostComponent, MobStateChangedEvent>(OnMobStateChanged);
     }
 
     private void OnClothingEquip(Entity<MonarchRayComponent> ent, ref ClothingGotEquippedEvent args)
     {
+        EnsureComp<MonarchRayHostComponent>(args.Wearer);
         ent.Comp.CurrentWearer = args.Wearer;
-
-        // grub steal mind
-        OnMonarchStealMind(ent);
-
-        // wearing the grub sets off this event. idk what to do with it yet but its here
-        var ev = new MonarchStealMindEvent(ent);
-        RaiseLocalEvent(ent, ref ev);
-        // test
     }
 
     private void OnClothingUnequip(Entity<MonarchRayComponent> ent, ref ClothingGotUnequippedEvent args)
     {
-        ent.Comp.CurrentWearer = null;
+        RemComp<MonarchRayHostComponent>(args.Wearer);
+        ent.Comp.CurrentWearer = args.Wearer;
+    }
+
+    private void OnMobStateChanged(Entity<MonarchRayHostComponent> ent, ref MobStateChangedEvent args)
+    {
+        // only go off when host dies
+        if (args.NewMobState != MobState.Dead)
+            return;
+
+        RemComp<MonarchRayHostComponent>(ent);
+
+        // if host is ACTUALLY wearing grub, fire mindsteal event
+        if (_inventorySystem.TryGetContainerSlotEnumerator(ent.Owner, out var enumerator, SlotFlags.HEAD))
+        {
+            while (enumerator.NextItem(out var item, out _))
+            {
+                if (!TryComp<MonarchRayComponent>(item, out var ray))
+                    continue;
+
+                OnMonarchStealMind((item, ray));
+            }
+        }
     }
 
     private void OnMonarchStealMind(Entity<MonarchRayComponent> ent)
@@ -60,7 +81,7 @@ public sealed class MonarchRaySystem : EntitySystem
 
         if (TryComp<ActorComponent>(wearer, out var actor) && actor.PlayerSession.GetMind() is { } mind)
         {
-            _mindSystem.TransferTo(mind, ent);
+            _mindSystem.TransferTo(mind, ent, ghostCheckOverride: true);
         }
 
         // KILL
