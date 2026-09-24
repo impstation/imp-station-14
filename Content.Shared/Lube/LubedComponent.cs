dd832a6826e75ed9a9ee780501281395eeeaf5a3
starlighthowls <starypaws7@gmail.com>
2026-09-24 21:03:11 +0100

    Update Content.Shared/Lube/LubedComponent.cs
    Co-authored-by: Jason <157201244+jkwookee@users.noreply.github.com>

--- Content.Shared/Lube/LubedComponent.cs
+++ Content.Shared/Lube/LubedComponent.cs
@@ -9,10 +9,10 @@ public sealed partial class LubedComponent : Component
     [DataField("slipStrength"), ViewVariables(VVAccess.ReadWrite)]
     public int SlipStrength;
 
-   /// <summary>
-   /// Imp addition. Controls if "lubed" is added to the start of lubed entities' names.
-   /// </summary>
-   [DataField]
-   public bool ApplyNamePrefix = true;
+    /// <summary>
+    /// Imp addition. Controls if "lubed" is added to the start of lubed entities' names.
+    /// </summary>
+    [DataField]
+    public bool ApplyNamePrefix = true;
 
 }
